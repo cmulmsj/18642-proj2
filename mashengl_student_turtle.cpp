7857b24f2077e4d7a34f5c6ed0db60e440c847bb
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-06 22:45:05 -0400

    Replaced magic numbers with constants and enums for clarity.

--- mashengl_student_turtle.cpp
+++ mashengl_student_turtle.cpp
@@ -19,6 +19,19 @@ turtleMove studentTurtleStep(bool bumped) { return MOVE; }
 // OK TO MODIFY BELOW THIS LINE
 
 #define TIMEOUT 40    // bigger number slows down simulation so you can see what's happening
+// Enum for directions
+enum Direction {
+    LEFT = 0,
+    UP = 1,
+    RIGHT = 2,
+    DOWN = 3
+};
+
+// States
+const int STATE_MOVING = 2;
+const int STATE_TURNED = 1;
+const int STATE_BUMPED = 0;
+
 float timeoutCounter, currentState;
 float futureX, futureY, futureX2, futureY2;
 float z, atEnd, mod, isBumped, q;
@@ -28,22 +41,22 @@ float z, atEnd, mod, isBumped, q;
 // Ground rule -- you are only allowed to call the helper functions "bumped(..)" and "atend(..)",
 // and NO other turtle methods or maze methods (no peeking at the maze!)
 int updateOrientation(int orientation, bool isBumped, float& currentState) {
-    if (orientation == 0) {
-        if (currentState == 2) { orientation = 1; currentState = 1; }  // Try right
-        else if (isBumped)     { orientation = 3; currentState = 0; }  // If bumped, go left
-        else currentState = 2;                                          // Otherwise, go straight
-    } else if (orientation == 1) {
-        if (currentState == 2) { orientation = 2; currentState = 1; }
-        else if (isBumped)     { orientation = 0; currentState = 0; }
-        else currentState = 2;
-    } else if (orientation == 2) {
-        if (currentState == 2) { orientation = 3; currentState = 1; }
-        else if (isBumped)     { orientation = 1; currentState = 0; }
-        else currentState = 2;
-    } else if (orientation == 3) {
-        if (currentState == 2) { orientation = 0; currentState = 1; }
-        else if (isBumped)     { orientation = 2; currentState = 0; }
-        else currentState = 2;
+    if (orientation == LEFT) {
+        if (currentState == STATE_MOVING) { orientation = UP; currentState = STATE_TURNED; }  // Try right
+        else if (isBumped)                { orientation = DOWN; currentState = STATE_BUMPED; }  // If bumped, go left
+        else currentState = STATE_MOVING;                                              // Otherwise, go straight
+    } else if (orientation == UP) {
+        if (currentState == STATE_MOVING) { orientation = RIGHT; currentState = STATE_TURNED; }
+        else if (isBumped)                { orientation = LEFT; currentState = STATE_BUMPED; }
+        else currentState = STATE_MOVING;
+    } else if (orientation == RIGHT) {
+        if (currentState == STATE_MOVING) { orientation = DOWN; currentState = STATE_TURNED; }
+        else if (isBumped)                { orientation = UP; currentState = STATE_BUMPED; }
+        else currentState = STATE_MOVING;
+    } else if (orientation == DOWN) {
+        if (currentState == STATE_MOVING) { orientation = LEFT; currentState = STATE_TURNED; }
+        else if (isBumped)                { orientation = RIGHT; currentState = STATE_BUMPED; }
+        else currentState = STATE_MOVING;
     }
     return orientation;
 }
@@ -57,13 +70,13 @@ bool studentMoveTurtle(QPointF& pos_, int& nw_or)
         futureX = pos_.x(); futureY = pos_.y();
         futureX2 = pos_.x(); futureY2 = pos_.y();
 
-        if (nw_or < 2) {
-            if (nw_or == 0) futureY2 += 1;
-            else            futureX2 += 1;
-        } else {
+        if (nw_or < RIGHT) {  
+            if (nw_or == LEFT) futureY2 += 1;
+            else               futureX2 += 1;
+        } else {  
             futureX2 += 1; futureY2 += 1;
-            if (nw_or == 2) futureX += 1;
-            else            futureY += 1;
+            if (nw_or == RIGHT) futureX += 1;
+            else                futureY += 1;
         }
 
         isBumped = bumped(futureX, futureY, futureX2, futureY2);
@@ -73,22 +86,22 @@ bool studentMoveTurtle(QPointF& pos_, int& nw_or)
 
         ROS_INFO("Orientation=%f  STATE=%f", nw_or, currentState);
 
-        if (currentState == 2 && !atEnd) {
+        if (currentState == STATE_MOVING && !atEnd) {
             switch (nw_or) {
-                case 0: pos_.setX(pos_.x() - 1); break;  // Move left
-                case 1: pos_.setY(pos_.y() - 1); break;  // Move up
-                case 2: pos_.setX(pos_.x() + 1); break;  // Move right
-                case 3: pos_.setY(pos_.y() + 1); break;  // Move down
+                case LEFT:  pos_.setX(pos_.x() - 1); break;  // Move left
+                case UP:    pos_.setY(pos_.y() - 1); break;  // Move up
+                case RIGHT: pos_.setX(pos_.x() + 1); break;  // Move right
+                case DOWN:  pos_.setY(pos_.y() + 1); break;  // Move down
             }
             mod = true;
         }
 
         if (atEnd) return false;
 
-        timeoutCounter = TIMEOUT;
+        timeoutCounter = DEFAULT_TIMEOUT;
     } else {
         timeoutCounter -= 1;
     }
 
-    return (timeoutCounter == TIMEOUT);
+    return (timeoutCounter == DEFAULT_TIMEOUT);
 }
