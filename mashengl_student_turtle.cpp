db6be2b9bb1745b10fd86f9b586353d9ec945af8
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 05:51:35 -0400

    Update mashengl_student_turtle.cpp

--- mashengl_student_turtle.cpp
+++ mashengl_student_turtle.cpp
@@ -17,43 +17,47 @@
 const int MAZE_SIZE = 23;
 const int START_POS = MAZE_SIZE / 2;
 
-enum class turtleMove {
-    FORWARD,
-    TURN_LEFT,
-    TURN_RIGHT,
-    NO_MOVE
-};
-
-enum class Direction {
-    LEFT,
-    UP,
-    RIGHT,
-    DOWN
-};
-
 static std::vector<std::vector<int>> visitMap(MAZE_SIZE, std::vector<int>(MAZE_SIZE, 0));
 static int currentX = START_POS;
 static int currentY = START_POS;
-static Direction currentDirection = Direction::UP;
+static int currentOrientation = 1; // Start facing up (0: Left, 1: Up, 2: Right, 3: Down)
 
-turtleMove studentTurtleStep(bool bumped) {
+turtleMove studentTurtleStep(bool bumped)
+{
     if (bumped) {
-        currentDirection = static_cast<Direction>((static_cast<int>(currentDirection) + 1) % 4);
-        return turtleMove::TURN_RIGHT;
+        // Turn right when bumped
+        currentOrientation = (currentOrientation + 1) % 4;
+    } else {
+        // Move forward
+        switch (currentOrientation) {
+            case 0: currentX--; break; // Left
+            case 1: currentY--; break; // Up
+            case 2: currentX++; break; // Right
+            case 3: currentY++; break; // Down
+        }
+        
+        visitMap[currentY][currentX]++;
     }
     
-    // Move forward
-    switch (currentDirection) {
-        case Direction::LEFT:  currentX--; break;
-        case Direction::UP:    currentY--; break;
-        case Direction::RIGHT: currentX++; break;
-        case Direction::DOWN:  currentY++; break;
-    }
-    
-    visitMap[currentY][currentX]++;
-    return turtleMove::FORWARD;
+    return MOVE;
 }
 
-int getVisitsFromTurtle(int x, int y) {
-    return visitMap[y][x];
+int getVisitsFromTurtle(int x, int y)
+{
+    // Convert absolute coordinates to turtle's local coordinates
+    int localX = x - START_POS + currentX;
+    int localY = y - START_POS + currentY;
+    
+    // Check if the coordinates are within the visitMap bounds
+    if (localX >= 0 && localX < MAZE_SIZE && localY >= 0 && localY < MAZE_SIZE) {
+        return visitMap[localY][localX];
+    }
+    return 0; // Return 0 for out-of-bounds coordinates
+}
+
+// This function is no longer needed, but keeping it here as per the student.h file
+bool studentMoveTurtle(QPointF& pos_, int& nw_or)
+{
+    // This function is now empty as its functionality has been moved to moveTurtle in mashengl_student_maze.cpp
+    return true;
 }
