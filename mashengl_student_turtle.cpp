e5250a063c6af2565a4ede0201d617f9082050ee
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 16:27:21 -0400

    Update mashengl_student_turtle.cpp

--- mashengl_student_turtle.cpp
+++ mashengl_student_turtle.cpp
@@ -51,8 +51,6 @@ void setVisitCount(int32_t x, int32_t y, int32_t count) {
  * Determines the turtle's next move based on whether it has bumped into a wall.
  */
 turtleMove studentTurtleStep(bool bumped) {
-    ROS_INFO("Orientation: %d, Position: (%d, %d), State: %d, Bumped: %d", orientation, currentX, currentY, state, bumped);
-
     static bool firstCall = true;
     if (firstCall) {
         // Initialize the visit count at the starting position
@@ -64,6 +62,10 @@ turtleMove studentTurtleStep(bool bumped) {
     static int state = 0; // 0: Try to turn right, 1: Move forward, 2: Turn left
 
     turtleMove nextMove;
+
+    // Debugging statement placed after 'state' declaration
+    ROS_INFO("Orientation: %d, Position: (%d, %d), State: %d, Bumped: %d",
+             orientation, currentX, currentY, state, bumped);
 
     if (state == 0) {
         // Try to turn right
@@ -119,5 +121,7 @@ turtleMove studentTurtleStep(bool bumped) {
             state = 0;
         }
     }
+
     return nextMove;
 }
+
