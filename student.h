b6c3ffbd98b9e26ea78162852766f53cad24b02d
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 15:31:20 -0400

    Update student.h

--- student.h
+++ student.h
@@ -1,25 +1,24 @@
-#include <ros/ros.h>
-#include <boost/bind.hpp>
-#include <ece642rtle/timeInt8.h>
-#include <std_msgs/Empty.h>
-#include <ece642rtle/RTIbump.h>
-#include <ece642rtle/RTIatend.h>
-#include <ece642rtle/PoseOrntBundle.h>
-#include <ece642rtle/bumpEcho.h>
-#include <ece642rtle/aendEcho.h>
+#ifndef STUDENT_H
+#define STUDENT_H
+
 #include <QPointF>
 
-// Functions to interface with ROS. Don't change these lines!
-bool bumped(int x1,int y1,int x2,int y2);
-bool atend(int x, int y);
-void displayVisits(int visits);
-bool moveTurtle(QPointF& pos_, int& nw_or);
+// Enum to define turtle's possible moves
+enum turtleMove { FORWARD, LEFT, RIGHT };
 
-// Scope-preserving changes to these lines permitted (see p5 writeup)
-enum turtleMove {MOVE};
+// Function declarations
+bool moveTurtle(QPointF& pos_, int& nw_or);
+turtleMove studentTurtleStep(bool bumped);
 QPointF translatePos(QPointF pos_, turtleMove nextMove);
 int translateOrnt(int orientation, turtleMove nextMove);
-turtleMove studentTurtleStep(bool bumped);
 
-// OK to change below this line
-bool studentMoveTurtle(QPointF& pos_, int& nw_or);
+// Wall-checking and goal functions (simulated by the maze logic)
+bool bumped(int x1, int y1, int x2, int y2);
+bool atEnd(int x, int y);
+void displayVisits(int visits);
+
+// Getter and setter for the visit count in the local map
+int getVisitCount(int x, int y);
+void setVisitCount(int x, int y, int visits);
+
+#endif // STUDENT_H
