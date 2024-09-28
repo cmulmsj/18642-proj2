8636bb265b932c1d5b55ce6660bca70f6aa88242
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 17:32:02 -0400

    Update mashengl_student_turtle.cpp

--- mashengl_student_turtle.cpp
+++ mashengl_student_turtle.cpp
@@ -54,55 +54,37 @@ void setVisitCount(int32_t x, int32_t y, int32_t count) {
  * Determines the turtle's next move based on whether it has bumped into a wall.
  */
 turtleMove studentTurtleStep(bool bumped) {
-    static int state = 0; // 0: Check right, 1: Move forward, 2: Check left, 3: Turn around
+    static int state = 0; // 0: Move forward, 1: Turn right, 2: Turn left
     static bool firstMove = true;
 
-    ROS_INFO("Turtle Input - Bumped: %d, State: %d, First Move: %d",
-             bumped, state, firstMove);
+    ROS_INFO("Turtle Input - Bumped: %d, State: %d, First Move: %d, Orientation: %d",
+             bumped, state, firstMove, orientation);
 
     turtleMove nextMove;
 
     if (firstMove) {
         nextMove = FORWARD;
         firstMove = false;
+        state = 1; // Next, we'll try to turn right
     } else {
         switch (state) {
-            case 0: // Check right
+            case 0: // Moving forward
+                if (bumped) {
+                    nextMove = TURN_RIGHT;
+                    state = 1;
+                } else {
+                    nextMove = FORWARD;
+                    state = 1; // Always try to turn right after moving forward
+                }
+                break;
+            case 1: // Turning right
                 nextMove = TURN_RIGHT;
-                state = 1; // Next, try to move forward
+                state = 0; // After turning right, try to move forward
                 break;
-            case 1: // Try to move forward
-                if (!bumped) {
-                    nextMove = FORWARD;
-                    state = 0; // If successful, check right again
-                } else {
-                    nextMove = TURN_LEFT;
-                    state = 2; // If bumped, check left
-                }
-                break;
-            case 2: // Check left
-                if (!bumped) {
-                    nextMove = FORWARD;
-                    state = 0; // If path is clear, move forward
-                } else {
-                    nextMove = TURN_LEFT;
-                    state = 3; // If still bumped, prepare to turn around
-                }
-                break;
-            case 3: // Turn around
+            case 2: // Turning left
                 nextMove = TURN_LEFT;
-                state = 1; // After turning around, try to move forward
+                state = 0; // After turning left, try to move forward
                 break;
-        }
-    }
-
-    // Update local position based on move (only if actually moving)
-    if (nextMove == FORWARD && !bumped) {
-        switch (orientation) {
-            case LEFT: currentX--; break;
-            case RIGHT: currentX++; break;
-            case UP: currentY--; break;
-            case DOWN: currentY++; break;
         }
     }
 
@@ -113,11 +95,21 @@ turtleMove studentTurtleStep(bool bumped) {
         orientation = static_cast<Direction>((orientation + 1) % 4);
     }
 
+    // Update local position based on move (only if actually moving forward and not bumped)
+    if (nextMove == FORWARD && !bumped) {
+        switch (orientation) {
+            case LEFT: currentX--; break;
+            case RIGHT: currentX++; break;
+            case UP: currentY--; break;
+            case DOWN: currentY++; break;
+        }
+    }
+
     // Update visit count
     int visits = getVisitCount(currentX, currentY) + 1;
     setVisitCount(currentX, currentY, visits);
 
-    ROS_INFO("Turtle Decision - Next Move: %d, New State: %d, Local Pos: (%d, %d), Orientation: %d",
+    ROS_INFO("Turtle Decision - Next Move: %d, New State: %d, Local Pos: (%d, %d), New Orientation: %d",
              nextMove, state, currentX, currentY, orientation);
 
     return nextMove;
