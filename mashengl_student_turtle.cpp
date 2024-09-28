6c07ec3765633eb815600c8ef1027c102f1cb037
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 17:27:49 -0400

    Update mashengl_student_turtle.cpp

--- mashengl_student_turtle.cpp
+++ mashengl_student_turtle.cpp
@@ -95,30 +95,30 @@ turtleMove studentTurtleStep(bool bumped) {
                 break;
         }
     }
-}
 
-// Update local position based on move (only if actually moving)
-if (nextMove == FORWARD && !bumped) {
-    switch (orientation) {
-        case LEFT: currentX--; break;
-        case RIGHT: currentX++; break;
-        case UP: currentY--; break;
-        case DOWN: currentY++; break;
+    // Update local position based on move (only if actually moving)
+    if (nextMove == FORWARD && !bumped) {
+        switch (orientation) {
+            case LEFT: currentX--; break;
+            case RIGHT: currentX++; break;
+            case UP: currentY--; break;
+            case DOWN: currentY++; break;
+        }
     }
+
+    // Update orientation based on turn
+    if (nextMove == TURN_LEFT) {
+        orientation = static_cast<Direction>((orientation + 3) % 4);
+    } else if (nextMove == TURN_RIGHT) {
+        orientation = static_cast<Direction>((orientation + 1) % 4);
+    }
+
+    // Update visit count
+    int visits = getVisitCount(currentX, currentY) + 1;
+    setVisitCount(currentX, currentY, visits);
+
+    ROS_INFO("Turtle Decision - Next Move: %d, New State: %d, Local Pos: (%d, %d), Orientation: %d",
+             nextMove, state, currentX, currentY, orientation);
+
+    return nextMove;
 }
-
-// Update orientation based on turn
-if (nextMove == TURN_LEFT) {
-    orientation = static_cast<Direction>((orientation + 3) % 4);
-} else if (nextMove == TURN_RIGHT) {
-    orientation = static_cast<Direction>((orientation + 1) % 4);
-}
-
-// Update visit count
-int visits = getVisitCount(currentX, currentY) + 1;
-setVisitCount(currentX, currentY, visits);
-
-ROS_INFO("Turtle Decision - Next Move: %d, New State: %d, Local Pos: (%d, %d), Orientation: %d",
-         nextMove, state, currentX, currentY, orientation);
-
-return nextMove;
