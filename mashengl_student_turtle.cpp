c67ffe1d47eb136cc3d4fbb938b0d3fe151c77b0
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 08:08:47 -0400

    Update mashengl_student_turtle.cpp

--- mashengl_student_turtle.cpp
+++ mashengl_student_turtle.cpp
@@ -54,51 +54,66 @@ turtleMove studentTurtleStep(bool bumped) {
         STATE_TURN_LEFT
     } state = STATE_INIT;
 
+    ROS_INFO("Current state: %d, Bumped: %s", state, bumped ? "true" : "false");
+
+    turtleMove move;
     switch (state) {
         case STATE_INIT:
             state = STATE_CHECK_RIGHT;
             orientation = static_cast<Direction>((static_cast<int>(orientation) + 1) % 4);
-            return TURN_RIGHT;
+            move = TURN_RIGHT;
+            break;
 
         case STATE_CHECK_RIGHT:
             if (bumped) {
                 state = STATE_TURN_LEFT;
                 orientation = static_cast<Direction>((static_cast<int>(orientation) + 3) % 4);
-                return TURN_LEFT;
+                move = TURN_LEFT;
             } else {
                 state = STATE_MOVE_FORWARD;
-                return MOVE_FORWARD;
+                move = MOVE_FORWARD;
             }
+            break;
 
         case STATE_TURN_LEFT:
             state = STATE_CHECK_FORWARD;
             orientation = static_cast<Direction>((static_cast<int>(orientation) + 3) % 4);
-            return TURN_LEFT;
+            move = TURN_LEFT;
+            break;
 
         case STATE_CHECK_FORWARD:
             if (bumped) {
                 state = STATE_TURN_LEFT;
                 orientation = static_cast<Direction>((static_cast<int>(orientation) + 3) % 4);
-                return TURN_LEFT;
+                move = TURN_LEFT;
             } else {
                 state = STATE_MOVE_FORWARD;
-                return MOVE_FORWARD;
+                move = MOVE_FORWARD;
             }
+            break;
 
         case STATE_MOVE_FORWARD:
-            switch (orientation) {
-                case UP:    currentY -= 1; break;
-                case RIGHT: currentX += 1; break;
-                case DOWN:  currentY += 1; break;
-                case LEFT:  currentX -= 1; break;
+            if (!bumped) {
+                switch (orientation) {
+                    case UP:    currentY -= 1; break;
+                    case RIGHT: currentX += 1; break;
+                    case DOWN:  currentY += 1; break;
+                    case LEFT:  currentX -= 1; break;
+                }
+                setVisitCount(currentX, currentY, getVisitCount(currentX, currentY) + 1);
             }
-            setVisitCount(currentX, currentY, getVisitCount(currentX, currentY) + 1);
             state = STATE_CHECK_RIGHT;
-            return MOVE_FORWARD;
+            move = MOVE_FORWARD;
+            break;
 
         default:
             ROS_ERROR("Invalid state in studentTurtleStep");
             state = STATE_INIT;
-            return STOP;
+            move = STOP;
+            break;
     }
+
+    ROS_INFO("Next move: %d, New state: %d, New orientation: %d", 
+             static_cast<int>(move), state, static_cast<int>(orientation));
+    return move;
 }
