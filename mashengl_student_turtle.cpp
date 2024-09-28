146995aab3f36e697fda94fc39c3b75dee8dc3c0
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 16:04:58 -0400

    Update mashengl_student_turtle.cpp

--- mashengl_student_turtle.cpp
+++ mashengl_student_turtle.cpp
@@ -30,10 +30,13 @@ static int32_t currentY = START_POS; // Turtle's current Y position in local map
 enum Direction { UP = 0, RIGHT = 1, DOWN = 2, LEFT = 3 };
 static Direction orientation = UP; // Assuming the turtle starts facing UP
 
+// Wall map to keep track of detected walls
+static bool wallMap[MAZE_SIZE][MAZE_SIZE][4] = {false}; // [x][y][direction]
+
 // Function prototypes
 int32_t getVisitCount(int32_t x, int32_t y);
 void setVisitCount(int32_t x, int32_t y, int32_t count);
-
+void updateAfterMove(bool moveSuccessful);
 /**
  * Retrieves the visit count for a specific cell in the local map.
  */
@@ -54,30 +57,38 @@ void setVisitCount(int32_t x, int32_t y, int32_t count) {
  */
 turtleMove studentTurtleStep(bool bumped) {
     static bool firstCall = true;
-    static int state = 0; // 0: Try to turn right, 1: Move forward, 2: Turn left
-    static int turnCount = 0; // To prevent infinite turning
-
     if (firstCall) {
-        // Initialize the visit count at the starting position
         setVisitCount(currentX, currentY, 1);
         firstCall = false;
     }
 
+    // Update wall map if bumped
     if (bumped) {
-        // Can't move forward, need to turn left
-        orientation = static_cast<Direction>((orientation + 3) % 4); // Turn left
-        turnCount++;
-        if (turnCount > 4) {
-            // We've turned all directions and are stuck
-            return STOP;
-        }
-        return TURN_LEFT;
-    } else {
-        // Reset turn count since we're able to move
-        turnCount = 0;
+        wallMap[currentX][currentY][orientation] = true;
+    }
 
+    // Right-hand wall-following logic
+    // Try to turn right
+    Direction rightDir = static_cast<Direction>((orientation + 1) % 4);
+    bool wallOnRight = wallMap[currentX][currentY][rightDir];
+
+    if (!wallOnRight) {
+        // Turn right and move forward
+        orientation = rightDir;
+        return TURN_RIGHT;
+    } else if (!bumped) {
         // Move forward
-        // Update the turtle's position based on its orientation
+        return FORWARD;
+    } else {
+        // Turn left
+        orientation = static_cast<Direction>((orientation + 3) % 4);
+        return TURN_LEFT;
+    }
+}
+
+void updateAfterMove(bool moveSuccessful) {
+    if (moveSuccessful) {
+        // Update the turtle's internal position
         switch (orientation) {
             case UP:    currentY--; break;
             case RIGHT: currentX++; break;
@@ -88,7 +99,9 @@ turtleMove studentTurtleStep(bool bumped) {
         // Update visit count
         int32_t visits = getVisitCount(currentX, currentY) + 1;
         setVisitCount(currentX, currentY, visits);
-
-        return FORWARD;
+    } else {
+        // Since move failed, mark wall ahead
+        wallMap[currentX][currentY][orientation] = true;
     }
+}
 }
