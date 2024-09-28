3af17113a3b76099fe27749bd14a69573a9bbbf2
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 06:42:23 -0400

    Update student.h

--- student.h
+++ student.h
@@ -13,12 +13,12 @@
 #include <QPointF>
 
 // Functions to interface with ROS. Don't change these lines!
-bool bumped(int x1, int y1, int x2, int y2);
+bool bumped(int x1,int y1,int x2,int y2);
 bool atend(int x, int y);
 void displayVisits(int visits);
 bool moveTurtle(QPointF& pos_, int& nw_or);
 
-// Scope-preserving changes to these lines permitted (see project writeup)
+// Scope-preserving changes to these lines permitted
 enum turtleMove { MOVE_FORWARD, TURN_LEFT, TURN_RIGHT, STOP };
 turtleMove studentTurtleStep(bool bumped);
 void translatePos(QPointF& pos_, int nw_or, turtleMove nextMove);
@@ -26,5 +26,7 @@ void translateOrnt(int& nw_or, turtleMove nextMove);
 int getCurrentVisitCount();
 
 // OK to change below this line
+// No further changes needed
 
-#endif  // STUDENT_H
+#endif // STUDENT_H
+
