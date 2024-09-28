ce60e0c98346032c15664edf0a6094834d919181
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 08:00:28 -0400

    Update mashengl_student_turtle.cpp

--- mashengl_student_turtle.cpp
+++ mashengl_student_turtle.cpp
@@ -43,65 +43,40 @@ int getCurrentVisitCount() {
     return getVisitCount(currentX, currentY);
 }
 
-// The turtle's movement logic implementing the right-hand rule
+// In mashengl_student_turtle.cpp
+
 turtleMove studentTurtleStep(bool bumped) {
     static enum TurtleState {
-        STATE_START,
+        STATE_INIT,
         STATE_CHECK_RIGHT,
         STATE_MOVE_FORWARD,
         STATE_CHECK_FORWARD,
         STATE_TURN_LEFT
-    } state = STATE_START;
-
-    static bool justTurned = false;
-
-    if (justTurned) {
-        // After a turn, we need to check for a wall ahead
-        if (bumped) {
-            // There's a wall ahead, need to turn left
-            state = STATE_TURN_LEFT;
-            justTurned = false;
-            return TURN_LEFT;
-        } else {
-            // No wall ahead, move forward
-            state = STATE_CHECK_RIGHT;
-            justTurned = false;
-            return MOVE_FORWARD;
-        }
-    }
+    } state = STATE_INIT;
 
     switch (state) {
-        case STATE_START:
-        case STATE_CHECK_RIGHT:
-            // Try to turn right
-            orientation = static_cast<Direction>((orientation + 1) % 4); // Turn right
-            state = STATE_MOVE_FORWARD;
-            justTurned = true;
+        case STATE_INIT:
+            // Start by attempting to turn right
+            state = STATE_CHECK_RIGHT;
             return TURN_RIGHT;
 
-        case STATE_MOVE_FORWARD:
-            // Move forward in the current orientation
-            switch (orientation) {
-                case UP:    currentY -= 1; break;
-                case RIGHT: currentX += 1; break;
-                case DOWN:  currentY += 1; break;
-                case LEFT:  currentX -= 1; break;
+        case STATE_CHECK_RIGHT:
+            if (bumped) {
+                // Wall to the right, turn left to original orientation
+                state = STATE_TURN_LEFT;
+                return TURN_LEFT;
+            } else {
+                // No wall to the right, move forward
+                state = STATE_MOVE_FORWARD;
+                return MOVE_FORWARD;
             }
-            // Increment visit count
-            setVisitCount(currentX, currentY, getVisitCount(currentX, currentY) + 1);
-            // After moving forward, check right again
-            state = STATE_CHECK_RIGHT;
-            return MOVE_FORWARD;
 
         case STATE_TURN_LEFT:
-            // Turn left to find a new path
-            orientation = static_cast<Direction>((orientation + 3) % 4); // Turn left
+            // After turning left, check forward
             state = STATE_CHECK_FORWARD;
-            justTurned = true;
             return TURN_LEFT;
 
         case STATE_CHECK_FORWARD:
-            // After turning left, check if we can move forward
             if (bumped) {
                 // Wall ahead, turn left again
                 state = STATE_TURN_LEFT;
@@ -112,9 +87,25 @@ turtleMove studentTurtleStep(bool bumped) {
                 return MOVE_FORWARD;
             }
 
+        case STATE_MOVE_FORWARD:
+            // Move forward in the current orientation
+            // Update internal position
+            switch (orientation) {
+                case UP:    currentY -= 1; break;
+                case RIGHT: currentX += 1; break;
+                case DOWN:  currentY += 1; break;
+                case LEFT:  currentX -= 1; break;
+            }
+            // Increment visit count
+            setVisitCount(currentX, currentY, getVisitCount(currentX, currentY) + 1);
+            // After moving forward, attempt to turn right again
+            state = STATE_CHECK_RIGHT;
+            return MOVE_FORWARD;
+
         default:
             ROS_ERROR("Invalid state in studentTurtleStep");
-            state = STATE_START;
+            state = STATE_INIT;
             return STOP;
     }
 }
+
