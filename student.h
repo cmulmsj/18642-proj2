97f5e511ed070e669ac8863a78240b736c9143ae
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 21:27:02 -0400

    Update student.h

--- student.h
+++ student.h
@@ -11,7 +11,6 @@
 #include <ece642rtle/bumpEcho.h>
 #include <ece642rtle/aendEcho.h>
 #include <QPointF>
-#include "mashengl_maze_params.h"
 
 // Functions to interface with ROS. Don't change these lines!
 bool bumped(int x1, int y1, int x2, int y2);
@@ -19,12 +18,37 @@ bool atend(int x, int y);
 void displayVisits(int visits);
 bool moveTurtle(QPointF& pos_, int& nw_or);
 
-QPointF translatePos(QPointF pos_, int orientation);
-int translateOrnt(int orientation, TurtleCommand nextMove);
-TurtleCommand studentTurtleStep(bool bumped, bool goal, NavigationMode* cur_state);
+enum turtleMove : int8_t {
+    MOVE,
+    TURNRIGHT,
+    TURNLEFT,
+    STOP
+};
 
-bool detectObstacle(QPointF pos_, int orient);
-void addVisit(QPointF& pos_);
-uint8_t getVisit(QPointF& pos_);
+enum Orientation : int8_t {
+    LEFT,
+    DOWN,
+    RIGHT,
+    UP
+};
+
+enum State : int8_t {
+    INIT,
+    GO,
+    TURN,
+    GOAL
+};
+
+QPointF translatePos(QPointF pos_, int orientation);
+int translateOrnt(int orientation, turtleMove nextMove);
+turtleMove studentTurtleStep(bool bumped, bool goal, State* cur_state);
+
+bool check_bumped(QPointF pos_, int orient);
+void record_visited(QPointF& pos_);
+uint8_t get_visited(QPointF& pos_);
+
+const uint8_t NUM_ORIENTATIONS = 4;
+const uint8_t TIMEOUT = 40;
+const uint8_t MAZE_SIZE = 23;
 
 #endif // STUDENT_H
