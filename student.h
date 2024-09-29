9e832b1452891d23870cbe51aa814d55916e80e1
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 23:38:26 -0400

    Update student.h

--- student.h
+++ student.h
@@ -12,46 +12,49 @@
 #include <ece642rtle/aendEcho.h>
 #include <QPointF>
 
-// Functions to interface with ROS. Don't change these lines!
+// Function prototypes to interface with ROS. Don't change these lines!
 bool bumped(int x1, int y1, int x2, int y2);
 bool atend(int x, int y);
 void displayVisits(int visits);
-bool moveTurtle(QPointF& pos_, int& nw_or);
+bool updateTurtle(QPointF& position, int& current_orientation);
 
-// Enumerations for turtle moves and orientations
-enum turtleMove : int8_t {
-    MOVE,
-    TURNRIGHT,
-    TURNLEFT,
-    STOP
+// Enumerations for turtle movements, orientations, and states
+enum class TurtleAction : int8_t {
+    ADVANCE,
+    ROTATE_RIGHT,
+    ROTATE_LEFT,
+    HALT
 };
 
-enum Orientation : int8_t {
-    LEFT,
-    DOWN,
-    RIGHT,
-    UP
+enum class CompassDirection : int8_t {
+    WEST,
+    SOUTH,
+    EAST,
+    NORTH
 };
 
-enum State : int8_t {
-    INIT,
-    GO,
-    TURN,
-    GOAL
+enum class TurtleState : int8_t {
+    INITIAL,
+    MOVING,
+    ADJUSTING,
+    GOAL_REACHED
 };
 
-// Function prototypes with updated naming conventions
-QPointF translatePos(QPointF pos_, Orientation orientation);
-int translateOrnt(int orientation, turtleMove nextMove);
-turtleMove studentTurtleStep(bool bumped, bool goal, State* cur_state);
+// Helper functions for position and orientation translations
+QPointF computeNextPosition(QPointF current_pos, CompassDirection direction);
+int updateOrientation(int orientation, TurtleAction action);
 
-// Renamed function prototypes
-bool detectObstacle(QPointF pos_, Orientation orient);
-void addVisit(QPointF& pos_);
-uint8_t getVisit(QPointF& pos_);
+// Turtle decision-making function
+TurtleAction determineNextAction(bool is_bumped, bool at_goal, TurtleState* current_state);
 
-const uint8_t NUM_ORIENTATIONS = 4;
-const uint8_t TIMEOUT = 40;
-const uint8_t MAZE_SIZE = 23;
+// Obstacle detection and visit tracking
+bool detectObstacle(QPointF current_pos, CompassDirection direction);
+void addVisit(QPointF& position);
+uint8_t retrieveVisitCount(QPointF& position);
+
+// Constants
+const uint8_t ORIENTATION_COUNT = 4;
+const uint8_t MOVE_DELAY = 10;
+const uint8_t MAZE_GRID_SIZE = 23;
 
 #endif // STUDENT_H
