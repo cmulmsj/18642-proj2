46d0957699ad07d0c0f7926f16bcb87fc943a171
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 23:58:13 -0400

    Update student.h

--- student.h
+++ student.h
@@ -12,47 +12,41 @@
 #include <ece642rtle/aendEcho.h>
 #include <QPointF>
 
-// Function prototypes to interface with ROS. Don't change these lines!
+// Functions to interface with ROS. Don't change these lines!
 bool bumped(int x1, int y1, int x2, int y2);
 bool atend(int x, int y);
 void displayVisits(int visits);
-bool updateTurtle(QPointF& position, int& current_orientation);
+bool moveTurtle(QPointF& pos_, int& nw_or);
 
-// Enumerations for turtle movements, orientations, and states
-enum class TurtleAction : int8_t {
-    ADVANCE,
-    ROTATE_RIGHT,
-    ROTATE_LEFT,
-    HALT
+enum turtleMove : int8_t {
+    MOVING,
+    TURNING_RIGHT,
+    TURNING_LEFT,
+    STOPPING
 };
 
-enum class CompassDirection : int8_t {
+enum Orientation : int8_t {
     WEST,
     SOUTH,
     EAST,
     NORTH
 };
 
-enum class TurtleState : int8_t {
-    INITIAL,
-    MOVING,
-    ADJUSTING,
-    GOAL_REACHED
+enum TurtleState : int8_t {
+    INIT,
+    GO,
+    TURN,
+    GOAL
 };
 
-// Helper functions for position and orientation translations
-QPointF computeNextPosition(QPointF current_pos, CompassDirection direction);
-int updateOrientation(int orientation, TurtleAction action);
+QPointF translatePos(QPointF pos_, Orientation orientation);
+int translateOrnt(int orientation, turtleMove nextMove);
+turtleMove studentTurtleStep(bool bumped, bool goal, TurtleState* cur_state);
 
-// Turtle decision-making function
-TurtleAction determineNextAction(bool is_bumped, bool at_goal, TurtleState* current_state);
+bool detectObstacle(QPointF pos_, Orientation orient);
+void addVisit(QPointF& pos_);
+uint8_t retrieveVisitCount(QPointF& pos_);
 
-// Obstacle detection and visit tracking
-bool detectObstacle(QPointF current_pos, CompassDirection direction);
-void addVisit(QPointF& position);
-uint8_t retrieveVisitCount(QPointF& position);
-
-// Constants
 const uint8_t ORIENTATION_COUNT = 4;
 const uint8_t MOVE_DELAY = 10;
 const uint8_t MAZE_GRID_SIZE = 23;
