ef9c615b4a4f3f069523c88879a5e431d1f63292
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-09-28 23:04:48 -0400

    Update mashengl_student_turtle.cpp

--- mashengl_student_turtle.cpp
+++ mashengl_student_turtle.cpp
@@ -13,59 +13,48 @@
 
 #include "student.h"
 
-static int8_t visit_map[MAZE_SIZE][MAZE_SIZE] = {0};
+static int8_t visited[MAZE_SIZE][MAZE_SIZE] = {0};
 
-// Renamed to addVisit as per your convention
-void addVisit(QPointF& pos_) {
-    int x = static_cast<int>(pos_.x());
-    int y = static_cast<int>(pos_.y());
-    if (x >= 0 && x < MAZE_SIZE && y >= 0 && y < MAZE_SIZE) {
-        visit_map[x][y]++;
-    }
+void record_visited(QPointF& pos_) {
+    visited[static_cast<int>(pos_.x())][static_cast<int>(pos_.y())]++;
 }
 
-// Renamed to getVisit as per your convention
-uint8_t getVisit(QPointF& pos_) {
-    int x = static_cast<int>(pos_.x());
-    int y = static_cast<int>(pos_.y());
-    if (x >= 0 && x < MAZE_SIZE && y >= 0 && y < MAZE_SIZE) {
-        return visit_map[x][y];
-    }
-    return 0;
+uint8_t get_visited(QPointF& pos_) {
+    return visited[static_cast<int>(pos_.x())][static_cast<int>(pos_.y())];
 }
 
-// Slightly modified logic for state transitions and movement
-TurtleMove studentTurtleStep(bool bumped, bool goal, NavigationMode* cur_state) {
-    TurtleMove nextMove;
+turtleMove studentTurtleStep(bool bumped, bool goal, State* cur_state) {
+    turtleMove nextMove;
 
     switch (*cur_state) {
-        case INITIAL:
-        case PROCEED:
+        case INIT:
+        case GO:
             if (goal) {
-                *cur_state = COMPLETE;
-                nextMove = HALT;
+                *cur_state = GOAL;
+                nextMove = STOP;
             } else {
-                *cur_state = ADJUST;
-                nextMove = ROTATE_CW; // Start by rotating clockwise to explore
+                *cur_state = TURN;
+                nextMove = TURNRIGHT;
             }
             break;
 
-        case ADJUST:
+        case TURN:
             if (bumped) {
-                nextMove = ROTATE_CCW; // Rotate counterclockwise if a wall is hit
+                *cur_state = TURN;
+                nextMove = TURNLEFT;
             } else {
-                *cur_state = PROCEED;
-                nextMove = ADVANCE; // Move forward if the path is clear
+                *cur_state = GO;
+                nextMove = MOVE;
             }
             break;
 
-        case COMPLETE:
-            nextMove = HALT;
+        case GOAL:
+            nextMove = STOP;
             break;
 
         default:
-            ROS_ERROR("Invalid Navigation Mode");
-            nextMove = HALT;
+            ROS_ERROR("Invalid State");
+            nextMove = STOP;
             break;
     }
 
