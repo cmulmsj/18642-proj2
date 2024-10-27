922ed8ad98cc58183a4554c36626ef95f2d1f208
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-10-27 14:34:26 -0400

    Update student.h

--- student.h
+++ student.h
@@ -18,42 +18,29 @@ bool atend(int x, int y);
 void displayVisits(int visits);
 bool moveTurtle(QPointF& pos_, int& nw_or);
 
-// Simple coordinate struct
+//enums for the possible turtleActions
+enum turtleAction {FORWARD, LEFT, RIGHT};
+
+//typedef for coordinates (x,y) in the map grid
 typedef struct {
-    uint8_t x;
-    uint8_t y;
+  uint8_t x;
+  uint8_t y;
 } coordinate;
 
-// Action enum - Kept simple for clarity
-enum turtleAction {
-    FORWARD,    // Move forward one cell
-    RIGHT,      // Turn right 90 degrees
-    LEFT,       // Turn left 90 degrees
-    NONE        // No action/stop
-};
-
-// Movement message struct
+//typedef for turtleMove message passed between turtle logic and maze navigation algorithms
+//contains action: to move FORWARD | LEFT | RIGHT
+//validAction: If the action is valid or not
+//visitCount: The number of visits it has made to this next square 
 typedef struct {
     turtleAction action;
     bool validAction;
     uint8_t visitCount;
 } turtleMove;
 
-// Core function declarations - Required interface
+void translatePosAndOrientation(turtleMove nextMove, QPointF& pos_, int& compass_orientation);
 QPointF translatePos(QPointF pos_, turtleMove nextMove, int compass_orientation);
 int translateOrnt(int orientation, turtleMove nextMove);
 turtleMove studentTurtleStep(bool bumped, bool at_end);
 
-// Helper function declarations
-uint8_t getVisits(coordinate pos);
-void setVisits(coordinate pos, uint8_t val);
-void updatePosition(coordinate& pos, uint8_t direction);
-bool checkWall(coordinate pos, uint8_t direction);
-
-// Constants for maze navigation
-constexpr uint8_t MAZE_SIZE = 30;
-constexpr uint8_t START_POS = 14;
-constexpr uint8_t MOVE_WAIT = 5;
-constexpr uint8_t MAX_DIRECTIONS = 4;
 
 #endif // STUDENT_H
