f65c4cf6ddd085aea65909a74781baada93ba48a
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-10-27 14:32:10 -0400

    Update mashengl_student_turtle.cpp

--- mashengl_student_turtle.cpp
+++ mashengl_student_turtle.cpp
@@ -9,15 +9,6 @@
 
 #include "student.h"
 
-// Visit tracking grid with center at START_POS
-static uint8_t visits[30][30] = {{0}};
-
-// Current position tracking
-static coordinate curr_pos = {14, 14};
-static LOCAL_DIR curr_dir = L_NORTH;
-static uint8_t dirs_checked = 0;
-static bool wall_status[4] = {false};
-
 // FSM States
 enum FSM_STATE { 
     FORWARD_STATE = 0,
@@ -33,6 +24,15 @@ enum LOCAL_DIR {
     L_SOUTH = 3 
 };
 
+// Visit tracking grid with center at START_POS
+static uint8_t visits[30][30] = {{0}};
+
+// Current position tracking
+static coordinate curr_pos = {14, 14};
+static uint8_t curr_dir = L_NORTH;  // Changed to uint8_t
+static uint8_t dirs_checked = 0;
+static bool wall_status[4] = {false};
+
 // Get visit count for a position
 uint8_t getVisits(coordinate pos) { 
     ROS_INFO("[VISIT] Get (%d,%d): %d", pos.x, pos.y, visits[pos.x][pos.y]);
@@ -46,7 +46,7 @@ void setVisits(coordinate pos, uint8_t val) {
 }
 
 // Get next position based on direction
-coordinate getNextPos(coordinate pos, LOCAL_DIR dir) {
+coordinate getNextPos(coordinate pos, uint8_t dir) {  // Changed parameter type
     coordinate next = pos;
     switch (dir) {
         case L_NORTH: next.y--; break;
@@ -58,7 +58,7 @@ coordinate getNextPos(coordinate pos, LOCAL_DIR dir) {
 }
 
 // Check if move forward is ideal (unvisited and no wall)
-bool isIdealMove(bool wall_ahead, coordinate pos, LOCAL_DIR dir) {
+bool isIdealMove(bool wall_ahead, coordinate pos, uint8_t dir) {  // Changed parameter type
     coordinate next = getNextPos(pos, dir);
     uint8_t visit_count = getVisits(next);
     ROS_INFO("[CHECK] Forward from (%d,%d) dir %d: wall=%d visits=%d", 
@@ -67,18 +67,18 @@ bool isIdealMove(bool wall_ahead, coordinate pos, LOCAL_DIR dir) {
 }
 
 // Find least visited accessible direction
-LOCAL_DIR getLeastVisited(coordinate pos, bool* walls) {
+uint8_t getLeastVisited(coordinate pos, bool* walls) {  // Changed return type
     uint8_t min_visits = 255;
-    LOCAL_DIR best_dir = L_NORTH;
+    uint8_t best_dir = L_NORTH;
     
     for(int i = 0; i < 4; i++) {
-        coordinate next = getNextPos(pos, static_cast<LOCAL_DIR>(i));
+        coordinate next = getNextPos(pos, i);
         uint8_t visits = getVisits(next);
         ROS_INFO("[SCAN] Dir %d - wall=%d visits=%d", i, walls[i], visits);
         
         if(!walls[i] && visits < min_visits) {
             min_visits = visits;
-            best_dir = static_cast<LOCAL_DIR>(i);
+            best_dir = i;
             ROS_INFO("[SCAN] New best direction: %d", i);
         }
     }
@@ -112,7 +112,7 @@ turtleMove studentTurtleStep(bool bumped, bool at_end) {
                 // Need to check other directions
                 state = CHECK_UNVISITED_STATE;
                 wall_status[curr_dir] = bumped;
-                curr_dir = static_cast<LOCAL_DIR>((curr_dir + 1) % 4);
+                curr_dir = (curr_dir + 1) % 4;
                 next_move.action = RIGHT;
                 dirs_checked = 1;
             }
@@ -122,7 +122,7 @@ turtleMove studentTurtleStep(bool bumped, bool at_end) {
         case CHECK_UNVISITED_STATE: {
             if(dirs_checked < 4) {
                 wall_status[curr_dir] = bumped;
-                curr_dir = static_cast<LOCAL_DIR>((curr_dir + 1) % 4);
+                curr_dir = (curr_dir + 1) % 4;
                 next_move.action = RIGHT;
                 dirs_checked++;
             } else {
@@ -134,9 +134,9 @@ turtleMove studentTurtleStep(bool bumped, bool at_end) {
         }
 
         case CHECK_UNBUMPED_STATE: {
-            LOCAL_DIR best_dir = getLeastVisited(curr_pos, wall_status);
+            uint8_t best_dir = getLeastVisited(curr_pos, wall_status);
             if(curr_dir != best_dir) {
-                curr_dir = static_cast<LOCAL_DIR>((curr_dir + 1) % 4);
+                curr_dir = (curr_dir + 1) % 4;
                 next_move.action = RIGHT;
             } else {
                 state = FORWARD_STATE;
