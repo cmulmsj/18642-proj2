fa2caca71681e2fcdd4233152ab163ecc53c8b07
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-10-27 14:01:15 -0400

    Update mashengl_student_turtle.cpp

--- mashengl_student_turtle.cpp
+++ mashengl_student_turtle.cpp
@@ -11,21 +11,21 @@
 
 // Track turtle's state between calls
 static struct TurtleData {
-    Point pos = {0, 0};                // Current position
     Orientation facing = NORTH;         // Current orientation
     Orientation target_dir = NORTH;     // Direction we want to face
-    bool completing_turn = false;       // True if in middle of turn sequence
 } turtle_data;
 
 // Find direction with least visits
-Orientation findBestDirection(QPointF current_pos, Orientation current_dir, bool is_bumped) {
-    int min_visits = 999;
-    Orientation best_dir = current_dir;
+Orientation findBestDirection(QPointF current_pos, bool is_bumped) {
+    uint8_t min_visits = 255;
+    Orientation best_dir = turtle_data.facing;
     
     // Check all directions
     for (int i = 0; i < ORIENTATION_COUNT; i++) {
-        if (i == current_dir && is_bumped) continue;
+        // Skip current direction if bumped
+        if (i == turtle_data.facing && is_bumped) continue;
         
+        // Get position in this direction
         QPointF test_pos = current_pos;
         switch (i) {
             case WEST:  test_pos.setX(current_pos.x() - 1); break;
@@ -39,7 +39,7 @@ Orientation findBestDirection(QPointF current_pos, Orientation current_dir, bool
             continue;
         }
         
-        int visits = retrieveVisitCount(test_pos);
+        uint8_t visits = retrieveVisitCount(test_pos);
         
         // Prefer unvisited cells
         if (visits == 0) {
@@ -55,6 +55,12 @@ Orientation findBestDirection(QPointF current_pos, Orientation current_dir, bool
     return best_dir;
 }
 
+// Get turn direction (LEFT or RIGHT) to reach target orientation
+turtleMove getTurnDirection(Orientation current, Orientation target) {
+    int diff = (target - current + ORIENTATION_COUNT) % ORIENTATION_COUNT;
+    return (diff == 1) ? TURNRIGHT : TURNLEFT;
+}
+
 // Main solver function
 turtleMove studentTurtleStep(bool bumped, bool goal, TurtleState* cur_state) {    
     if (goal) {
@@ -62,51 +68,47 @@ turtleMove studentTurtleStep(bool bumped, bool goal, TurtleState* cur_state) {
         return STOP;
     }
     
-    QPointF current_pos(turtle_data.pos.x, turtle_data.pos.y);
-    
     switch (*cur_state) {
         case EXPLORE: {
-            if (bumped) {
-                // Hit wall, need to turn
-                turtle_data.target_dir = findBestDirection(current_pos, turtle_data.facing, true);
+            // If bumped or at start, find a new direction
+            if (bumped || turtle_data.facing == turtle_data.target_dir) {
+                turtle_data.target_dir = findBestDirection(QPointF(0, 0), bumped);
+                
+                // If need to turn
                 if (turtle_data.target_dir != turtle_data.facing) {
                     *cur_state = TURNING;
-                    turtle_data.completing_turn = true;
-                    return ((turtle_data.target_dir - turtle_data.facing + ORIENTATION_COUNT) % ORIENTATION_COUNT == 1) ? 
-                           TURNRIGHT : TURNLEFT;
+                    return getTurnDirection(turtle_data.facing, turtle_data.target_dir);
                 }
-                return STOP;
             }
             
-            // Check for better direction
-            turtle_data.target_dir = findBestDirection(current_pos, turtle_data.facing, false);
-            if (turtle_data.target_dir != turtle_data.facing) {
-                *cur_state = TURNING;
-                turtle_data.completing_turn = true;
-                return ((turtle_data.target_dir - turtle_data.facing + ORIENTATION_COUNT) % ORIENTATION_COUNT == 1) ? 
-                       TURNRIGHT : TURNLEFT;
-            }
-            
-            // Move forward if no better direction
+            // Move forward if no turn needed
             return MOVE;
         }
         
         case TURNING: {
-            if (turtle_data.completing_turn) {
-                turtle_data.completing_turn = false;
-                turtle_data.facing = turtle_data.target_dir;
-                *cur_state = EXPLORE;
-                return MOVE;
+            // Update our orientation after turning
+            if (turtle_data.target_dir != turtle_data.facing) {
+                turtle_data.facing = static_cast<Orientation>(
+                    (turtle_data.facing + 
+                     (getTurnDirection(turtle_data.facing, turtle_data.target_dir) == TURNRIGHT ? 1 : 3)
+                    ) % ORIENTATION_COUNT
+                );
+                return STOP;
             }
-            return STOP;
+            
+            // Turn complete, start moving
+            *cur_state = EXPLORE;
+            return MOVE;
         }
         
-        case BACKTRACK:
-            // Simple backtracking - just turn right and try a different direction
+        case BACKTRACK: {
+            // If all else fails, turn right and try again
             turtle_data.facing = static_cast<Orientation>((turtle_data.facing + 1) % ORIENTATION_COUNT);
+            turtle_data.target_dir = turtle_data.facing;
             *cur_state = EXPLORE;
             return TURNRIGHT;
-            
+        }
+        
         case AT_END:
         default:
             return STOP;
