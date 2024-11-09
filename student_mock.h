9b9547bc288b9754d566e30d96c2a3c24bba0fa9
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-08 19:38:05 -0500

    Update student_mock.h

--- student_mock.h
+++ student_mock.h
@@ -2,6 +2,25 @@
 #define STUDENT_MOCK_H
 
 #include <stdint.h>
+#include <stdio.h>
+
+// Redefine ROS macros for testing
+#define ROS_INFO(format, ...) printf(format "\n", ##__VA_ARGS__)
+#define ROS_ERROR(format, ...) printf("ERROR: " format "\n", ##__VA_ARGS__)
+
+// Include all enums and types needed
+enum FSM_STATES { 
+    STATE_FORWARD = 0,           
+    STATE_UNVISITED = 1,   
+    STATE_UNBUMPED = 2  
+};
+
+enum LOCAL_DIRECTION { 
+    L_WEST = 0, 
+    L_NORTH = 1, 
+    L_EAST = 2, 
+    L_SOUTH = 3 
+};
 
 enum turtleAction {FORWARD, LEFT, RIGHT};
 
@@ -16,12 +35,29 @@ typedef struct {
     uint8_t visitCount;
 } turtleMove;
 
+// External variables that need to be accessible for testing
+extern FSM_STATES current_state;
+extern LOCAL_DIRECTION current_local_direction;
+extern coordinate current_location;
+extern uint8_t visit_count_map[30][30];
+
+// Mock functions declarations
 bool bumped(int x1, int y1, int x2, int y2);
 bool atend(int x, int y);
 void displayVisits(int visits);
 
+// Test control functions
 void setMockBump(bool will_bump);
 void setMockAtEnd(bool at_end);
 int getMockVisits();
+
+// Test access functions
+FSM_STATES getCurrentState();
+void setCurrentState(FSM_STATES state);
+LOCAL_DIRECTION getCurrentDirection();
+void setCurrentDirection(LOCAL_DIRECTION dir);
+coordinate getCurrentLocation();
+void setCurrentLocation(coordinate loc);
+void resetVisitMap();
 
 #endif
