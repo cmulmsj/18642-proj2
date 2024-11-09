b8024ab5919465a2d62f3ea790972ccce6cc0d2a
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-09 03:23:50 -0500

    Update student_mock.h

--- student_mock.h
+++ student_mock.h
@@ -1,30 +1,15 @@
+/*
+ * Student Name: Mashengjun Li
+ * Andrew ID: mashengl
+ */
+
 #ifndef STUDENT_MOCK_H
 #define STUDENT_MOCK_H
 
-#include <stdint.h>
-#include <stdio.h>
-
-#ifdef testing
-
-// ROS mock definitions
-#define ROS_INFO(format, ...) printf(format "\n", ##__VA_ARGS__)
-#define ROS_ERROR(format, ...) printf("ERROR: " format "\n", ##__VA_ARGS__)
-
-// Enums and types
-enum FSM_STATES { 
-    STATE_FORWARD = 0,           
-    STATE_UNVISITED = 1,   
-    STATE_UNBUMPED = 2  
-};
-
-enum LOCAL_DIRECTION { 
-    L_WEST = 0, 
-    L_NORTH = 1, 
-    L_EAST = 2, 
-    L_SOUTH = 3 
-};
+#include <cstdint>
 
 enum turtleAction {FORWARD, LEFT, RIGHT};
+enum RobotState {STARTUP, PLAN_NEXT, MOVING};
 
 typedef struct {
     uint8_t x;
@@ -37,45 +22,23 @@ typedef struct {
     uint8_t visitCount;
 } turtleMove;
 
-// Test state structure
-typedef struct {
-    FSM_STATES current_state;
-    LOCAL_DIRECTION current_direction;
-    coordinate current_location;
-    bool is_bumped;
-    bool is_at_end;
-    uint8_t timeout_counter;
-    uint8_t directions_checked;
-    uint8_t visit_count_map[30][30];
-} TurtleTestState;
-
-// External variables
-extern uint8_t visit_count_map[30][30];
-
-// Test control functions
-void initialize_test(void);
-void cleanup_test(void);
-void set_test_state(TurtleTestState state);
-TurtleTestState get_test_state(void);
-
-// Mock state access
-FSM_STATES getCurrentState(void);
-void setCurrentState(FSM_STATES state);
-LOCAL_DIRECTION getCurrentDirection(void);
-void setCurrentDirection(LOCAL_DIRECTION dir);
-coordinate getCurrentLocation(void);
-void setCurrentLocation(coordinate loc);
-void resetVisitMap(void);
-
-// Mock control functions
-void setMockBump(bool bump);
-void setMockAtEnd(bool at_end);
-uint8_t getMockVisitCount(void);
-bool getError(void);
-
-// Turtle functions
+// Core function to test
 turtleMove studentTurtleStep(bool bumped, bool at_end);
 
-#endif // testing
+// Mock functions for testing
+void mock_set_wall(bool has_wall);
+void mock_set_visit_count(int count);
+void mock_set_directional_visits(int north, int east, int south, int west);
+int getVisitCount(coordinate pos);
+int getExpectedX();
+int getExpectedY();
+
+// External state variables that need to be accessible
+extern RobotState robot_state;
+extern bool first_run;
+extern coordinate current_pos;
+extern int facing_direction;
+extern int rotations_checked;
+extern int best_direction;
 
 #endif // STUDENT_MOCK_H
