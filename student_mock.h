31eef562b83a1e7bbdfc590ede8bebd7966ed43e
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-09 04:15:17 -0500

    Update student_mock.h

--- student_mock.h
+++ student_mock.h
@@ -12,6 +12,7 @@
 // Constants
 #define GRID_SIZE 30
 #define START_POS 13
+#define UINT8_MAX 255
 
 // Mock QPointF
 class QPointF {
@@ -40,25 +41,28 @@ struct turtleMove {
     int visitCount;
 };
 
-// Main function that we're testing (implemented in student_turtle.cpp)
+// Main function that we're testing
 turtleMove studentTurtleStep(bool bumped, bool at_end);
 
-// Test control functions (defined in mock_functions.cpp)
+// Test control functions
 void mock_set_wall(bool has_wall);
 void mock_set_visit_count(int count);
 void mock_reset_state();
+void updateVisitMap(coordinate pos);
 
-// Original functions that need mocking (defined in mock_functions.cpp)
+// Original functions that need mocking
 bool checkObstacle(QPointF pos, int direction);
 int getVisitCount(coordinate pos);
 void ROS_INFO(const char* format, ...);
 
-// State variables (defined in mock_functions.cpp)
+// State variables
 extern RobotState robot_state;
 extern bool first_run;
 extern coordinate current_pos;
 extern int facing_direction;
 extern int rotations_checked;
 extern int best_direction;
+extern int min_visits;
+extern int visit_grid[GRID_SIZE][GRID_SIZE];
 
 #endif // STUDENT_MOCK_H
