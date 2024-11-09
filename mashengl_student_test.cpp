b06eb3ee1428c9b11d625e4f2851a1911b3611d0
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-09 04:18:59 -0500

    Update mashengl_student_test.cpp

--- mashengl_student_test.cpp
+++ mashengl_student_test.cpp
@@ -5,23 +5,55 @@
 
 #include "student_mock.h"
 #include <CUnit/Basic.h>
+#include <iostream>
 
+// Test startup to PLAN_NEXT transition (T1)
 void test_t1() {
     mock_reset_state();
     mock_set_wall(false);
     
     turtleMove result = studentTurtleStep(false, false);
+    
+    std::cout << "T1 test - Action: " << result.action 
+              << ", ValidAction: " << result.validAction << std::endl;
+    
     CU_ASSERT_EQUAL(result.action, FORWARD);
     CU_ASSERT_TRUE(result.validAction);
 }
 
+// Test response to wall detection (T2)
 void test_t2() {
     mock_reset_state();
     mock_set_wall(true);
     
+    // First call to initialize exploration
+    turtleMove init_result = studentTurtleStep(false, false);
+    
+    // Second call with wall detection
     turtleMove result = studentTurtleStep(true, false);
+    
+    std::cout << "T2 test - Action: " << result.action 
+              << ", ValidAction: " << result.validAction
+              << ", RobotState: " << robot_state 
+              << ", RotationsChecked: " << rotations_checked << std::endl;
+    
+    // In PLAN_NEXT state with wall detected, should turn RIGHT to explore
+    CU_ASSERT_EQUAL(robot_state, PLAN_NEXT);
     CU_ASSERT_EQUAL(result.action, RIGHT);
     CU_ASSERT_TRUE(result.validAction);
+}
+
+// Added coverage test for movement after rotation
+void test_t3() {
+    mock_reset_state();
+    mock_set_wall(false);
+    
+    // Complete rotation sequence
+    for(int i = 0; i < 4; i++) {
+        turtleMove result = studentTurtleStep(false, false);
+        std::cout << "T3 test rotation " << i << " - Action: " << result.action 
+                  << ", RotationsChecked: " << rotations_checked << std::endl;
+    }
 }
 
 int init() {
@@ -45,7 +77,8 @@ int main() {
     }
 
     if ((NULL == CU_add_test(pSuite, "test of transition T1", test_t1)) ||
-        (NULL == CU_add_test(pSuite, "test of transition T2", test_t2)))
+        (NULL == CU_add_test(pSuite, "test of transition T2", test_t2)) ||
+        (NULL == CU_add_test(pSuite, "test of transition T3", test_t3)))
     {
         CU_cleanup_registry();
         return CU_get_error();
