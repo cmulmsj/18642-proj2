c8ab6d4bbe4cd4071ba38c87c523b71877d94df8
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-08 19:28:40 -0500

    Update mashengl_build_run_tests.sh

--- mashengl_build_run_tests.sh
+++ mashengl_build_run_tests.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+echo "Building and running turtle navigation unit tests..."
+
+g++ -Dtesting -o student_tests \
+    mashengl_student_test.cpp \
+    student_turtle.cpp \
+    mock_functions.cpp \
+    -lcunit
+
+if [ $? -eq 0 ]; then
+    echo "Build successful. Running tests..."
+    ./student_tests
+else
+    echo "Build failed!"
+    exit 1
+fi
