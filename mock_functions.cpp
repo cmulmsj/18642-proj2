a5801f6a4d140b11a3077bfc745d30533f0742bd
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-09 03:24:44 -0500

    Update mock_functions.cpp

--- mock_functions.cpp
+++ mock_functions.cpp
@@ -1,98 +1,34 @@
-#ifdef testing
+/*
+ * Student Name: Mashengjun Li
+ * Andrew ID: mashengl
+ */
 
 #include "student_mock.h"
+#include <iostream>
 
-static TurtleTestState test_state;
-static bool error_occurred = false;
+static int mock_visit_count = 0;
+static bool mock_wall = false;
 
-void initialize_test(void) {
-    test_state.current_state = STATE_FORWARD;
-    test_state.current_direction = L_NORTH;
-    test_state.current_location.x = 14;
-    test_state.current_location.y = 14;
-    test_state.is_bumped = false;
-    test_state.is_at_end = false;
-    test_state.timeout_counter = 0;
-    test_state.directions_checked = 0;
-    
-    for(int i = 0; i < 30; i++) {
-        for(int j = 0; j < 30; j++) {
-            test_state.visit_count_map[i][j] = 0;
-        }
-    }
-    error_occurred = false;
+// Mock function for visit count
+int getVisitCount(coordinate pos) {
+    return mock_visit_count;
 }
 
-void set_test_state(TurtleTestState state) {
-    test_state.current_state = state.current_state;
-    test_state.current_direction = state.current_direction;
-    test_state.current_location = state.current_location;
-    test_state.is_bumped = state.is_bumped;
-    test_state.is_at_end = state.is_at_end;
-    test_state.timeout_counter = state.timeout_counter;
-    test_state.directions_checked = state.directions_checked;
-    
-    for(int i = 0; i < 30; i++) {
-        for(int j = 0; j < 30; j++) {
-            test_state.visit_count_map[i][j] = state.visit_count_map[i][j];
-        }
-    }
+// Mock function for wall detection
+bool checkObstacle(QPointF pos, int direction) {
+    return mock_wall;
 }
 
-void cleanup_test(void) {
-    initialize_test();
+// Test control functions
+void mock_set_wall(bool has_wall) {
+    mock_wall = has_wall;
 }
 
-TurtleTestState get_test_state(void) {
-    return test_state;
+void mock_set_visit_count(int count) {
+    mock_visit_count = count;
 }
 
-FSM_STATES getCurrentState(void) {
-    return test_state.current_state;
+// Dummy ROS_INFO implementation for testing
+void ROS_INFO(const char* format, ...) {
+    // Do nothing in tests
 }
-
-void setCurrentState(FSM_STATES state) {
-    test_state.current_state = state;
-}
-
-LOCAL_DIRECTION getCurrentDirection(void) {
-    return test_state.current_direction;
-}
-
-void setCurrentDirection(LOCAL_DIRECTION dir) {
-    test_state.current_direction = dir;
-}
-
-coordinate getCurrentLocation(void) {
-    return test_state.current_location;
-}
-
-void setCurrentLocation(coordinate loc) {
-    test_state.current_location = loc;
-}
-
-void setMockBump(bool bump) {
-    test_state.is_bumped = bump;
-}
-
-void setMockAtEnd(bool at_end) {
-    test_state.is_at_end = at_end;
-}
-
-void resetVisitMap(void) {
-    for(int i = 0; i < 30; i++) {
-        for(int j = 0; j < 30; j++) {
-            test_state.visit_count_map[i][j] = 0;
-        }
-    }
-}
-
-uint8_t getMockVisitCount(void) {
-    coordinate loc = getCurrentLocation();
-    return test_state.visit_count_map[loc.x][loc.y];
-}
-
-bool getError(void) {
-    return error_occurred;
-}
-#endif
