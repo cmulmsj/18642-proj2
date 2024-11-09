1786b3b6258c3f43f95361e2fd8dd4d485ac8825
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-09 05:33:44 -0500

    Update student_mock.h

--- student_mock.h
+++ student_mock.h
@@ -10,27 +10,15 @@
 #include <climits>
 #include <iostream>
 
+// Function declarations for mock logging
+void mock_ros_info(const char* format, ...);
+void mock_ros_error(const char* format, ...);
+
 // Constants
 #define GRID_SIZE 30
 #define START_POS 13
 
-// Mock ROS logging macros
-#define ROS_INFO(...) mock_ros_info(__VA_ARGS__)
-#define ROS_ERROR(...) mock_ros_error(__VA_ARGS__)
-
-// Mock QPointF
-class QPointF {
-public:
-    QPointF() : xpos(0), ypos(0) {}
-    QPointF(double x, double y) : xpos(x), ypos(y) {}
-    double x() const { return xpos; }
-    double y() const { return ypos; }
-    void setX(double x) { xpos = x; }
-    void setY(double y) { ypos = y; }
-private:
-    double xpos, ypos;
-};
-
+// Enums and types
 enum turtleAction {FORWARD, LEFT, RIGHT};
 enum RobotState {STARTUP, PLAN_NEXT, MOVING};
 
@@ -45,33 +33,31 @@ struct turtleMove {
     int visitCount;
 };
 
-// Main function that we're testing
-turtleMove studentTurtleStep(bool bumped, bool at_end);
+// Mock QPointF
+class QPointF {
+public:
+    QPointF() : xpos(0), ypos(0) {}
+    QPointF(double x, double y) : xpos(x), ypos(y) {}
+    double x() const { return xpos; }
+    double y() const { return ypos; }
+    void setX(double x) { xpos = x; }
+    void setY(double y) { ypos = y; }
+private:
+    double xpos, ypos;
+};
 
-// Test control functions
-void mock_set_wall(bool has_wall);
-bool mock_get_wall();  // Add this getter
-void mock_set_visit_count(int count);
-void mock_reset_state();
-void updateVisitMap(coordinate pos);
-
-// Original functions that need mocking
-bool checkObstacle(QPointF pos, int direction);
-int getVisitCount(coordinate pos);
-void ROS_INFO(const char* format, ...);
-
-// Add function declarations for mock logging
-void mock_ros_info(const char* format, ...);
-void mock_ros_error(const char* format, ...);
-
-// State variables
+// External variables
 extern RobotState robot_state;
 extern bool first_run;
 extern coordinate current_pos;
 extern int facing_direction;
 extern int rotations_checked;
 extern int best_direction;
-extern int min_visits;
-extern int visit_grid[GRID_SIZE][GRID_SIZE];
+
+// Function declarations
+bool checkObstacle(QPointF pos, int direction);
+turtleMove studentTurtleStep(bool bumped, bool at_end);
+int getVisitCount(coordinate pos);
+void updateVisitMap(coordinate pos);
 
 #endif // STUDENT_MOCK_H
