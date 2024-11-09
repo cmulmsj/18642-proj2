25cbda5adc9a353a554d07b05418406e62b76b9c
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-09 03:28:41 -0500

    Update mashengl_build_run_tests.sh

--- mashengl_build_run_tests.sh
+++ mashengl_build_run_tests.sh
@@ -3,15 +3,18 @@
 # Build and run unit tests for turtle implementation
 # Student: Mashengjun Li (mashengl)
 
-g++ -Dtesting -o student_tests \
+g++ -std=c++11 -Dtesting \
+    -I/opt/ros/kinetic/include \
+    -o mashengl_student_tests \
     mashengl_student_test.cpp \
-    mashengl_student_turtle.cpp \
+    student_turtle.cpp \
     mock_functions.cpp \
     -lcunit
 
+# Check if compilation succeeded
 if [ $? -eq 0 ]; then
     echo "Build successful, running tests..."
-    ./student_tests
+    ./mashengl_student_tests
 else
     echo "Build failed!"
     exit 1
