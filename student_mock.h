c74bc467d6830b4ce27ada8cf7eb1935abff62c2
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-09 04:07:20 -0500

    Update student_mock.h

--- student_mock.h
+++ student_mock.h
@@ -2,17 +2,18 @@
  * Student Name: Mashengjun Li
  * Andrew ID: mashengl
  */
+
 #ifndef STUDENT_MOCK_H
 #define STUDENT_MOCK_H
 
 #include <cstdint>
 #include <climits>
 
-// Constants from original student.h
+// Constants
 #define GRID_SIZE 30
 #define START_POS 13
 
-// Mock QPointF for testing
+// Mock QPointF
 class QPointF {
 public:
     QPointF() : xpos(0), ypos(0) {}
@@ -39,27 +40,12 @@ struct turtleMove {
     int visitCount;
 };
 
-// Core function to test
-turtleMove studentTurtleStep(bool bumped, bool at_end);
-
-// Mock functions for testing
+// Test control functions (defined in mock_functions.cpp)
 void mock_set_wall(bool has_wall);
 void mock_set_visit_count(int count);
-void mock_set_directional_visits(int north, int east, int south, int west);
-int getVisitCount(coordinate pos);
-int getExpectedX();
-int getExpectedY();
+void mock_reset_state();
+
+// Original functions that need mocking (defined in mock_functions.cpp)
 bool checkObstacle(QPointF pos, int direction);
-
-// ROS mock
 void ROS_INFO(const char* format, ...);
-
-// External state variables that need to be accessible
-extern RobotState robot_state;
-extern bool first_run;
-extern coordinate current_pos;
-extern int facing_direction;
-extern int rotations_checked;
-extern int best_direction;
-
 #endif // STUDENT_MOCK_H
