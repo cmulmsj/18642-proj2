071327a484878b455e26147ede573fc387613be5
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-08 19:41:54 -0500

    Update student_mock.h

--- student_mock.h
+++ student_mock.h
@@ -4,24 +4,11 @@
 #include <stdint.h>
 #include <stdio.h>
 
-// Redefine ROS macros for testing
 #define ROS_INFO(format, ...) printf(format "\n", ##__VA_ARGS__)
 #define ROS_ERROR(format, ...) printf("ERROR: " format "\n", ##__VA_ARGS__)
 
-// Include all enums and types needed
-enum FSM_STATES { 
-    STATE_FORWARD = 0,           
-    STATE_UNVISITED = 1,   
-    STATE_UNBUMPED = 2  
-};
-
-enum LOCAL_DIRECTION { 
-    L_WEST = 0, 
-    L_NORTH = 1, 
-    L_EAST = 2, 
-    L_SOUTH = 3 
-};
-
+enum FSM_STATES;
+enum LOCAL_DIRECTION;
 enum turtleAction {FORWARD, LEFT, RIGHT};
 
 typedef struct {
