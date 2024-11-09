dd61077e4fa56511ec97bef399ee1b535c8d5f7f
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-08 20:48:00 -0500

    Update mock_functions.cpp

--- mock_functions.cpp
+++ mock_functions.cpp
@@ -1,29 +1,85 @@
 #include "student_mock.h"
 
-static bool mock_bump = false;
-static bool mock_at_end = false;
-static int mock_visits = 0;
+static TurtleTestState test_state;
+static bool error_occurred = false;
 
-bool bumped(int x1, int y1, int x2, int y2) {
-    return mock_bump;
+void initialize_test(void) {
+    test_state = {
+        .current_state = STATE_FORWARD,
+        .current_direction = L_NORTH,
+        .current_location = {14, 14},
+        .is_bumped = false,
+        .is_at_end = false,
+        .timeout_counter = 0,
+        .directions_checked = 0
+    };
+    
+    // Initialize visit map
+    for(int i = 0; i < 30; i++) {
+        for(int j = 0; j < 30; j++) {
+            test_state.visit_count_map[i][j] = 0;
+        }
+    }
+    error_occurred = false;
 }
 
-bool atend(int x, int y) {
-    return mock_at_end;
+void cleanup_test(void) {
+    initialize_test();
 }
 
-void displayVisits(int visits) {
-    mock_visits = visits;
+void set_test_state(TurtleTestState state) {
+    test_state = state;
 }
 
-void setMockBump(bool will_bump) {
-    mock_bump = will_bump;
+TurtleTestState get_test_state(void) {
+    return test_state;
+}
+
+FSM_STATES getCurrentState(void) {
+    return test_state.current_state;
+}
+
+void setCurrentState(FSM_STATES state) {
+    test_state.current_state = state;
+}
+
+LOCAL_DIRECTION getCurrentDirection(void) {
+    return test_state.current_direction;
+}
+
+void setCurrentDirection(LOCAL_DIRECTION dir) {
+    test_state.current_direction = dir;
+}
+
+coordinate getCurrentLocation(void) {
+    return test_state.current_location;
+}
+
+void setCurrentLocation(coordinate loc) {
+    test_state.current_location = loc;
+}
+
+void setMockBump(bool bump) {
+    test_state.is_bumped = bump;
 }
 
 void setMockAtEnd(bool at_end) {
-    mock_at_end = at_end;
+    test_state.is_at_end = at_end;
 }
 
-int getMockVisits() {
-    return mock_visits;
+void resetVisitMap(void) {
+    for(int i = 0; i < 30; i++) {
+        for(int j = 0; j < 30; j++) {
+            test_state.visit_count_map[i][j] = 0;
+        }
+    }
+}
+
+uint8_t getMockVisitCount(void) {
+    coordinate loc = getCurrentLocation();
+    return test_state.visit_count_map[loc.x][loc.y];
+}
+
+bool getError(void) {
+    return error_occurred;
 }
