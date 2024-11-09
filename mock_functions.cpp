c05a0900b3311a716e7db3074b643d70ef5eca52
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-09 04:12:11 -0500

    Update mock_functions.cpp

--- mock_functions.cpp
+++ mock_functions.cpp
@@ -7,11 +7,19 @@
 #include <iostream>
 #include <cstdarg>
 
+// State variables
+RobotState robot_state = STARTUP;
+bool first_run = true;
+coordinate current_pos = {START_POS, START_POS};
+int facing_direction = 1;
+int rotations_checked = 0;
+int best_direction = -1;
+
 // Mock state variables
 static bool mock_wall = false;
 static int mock_visit_count = 0;
 
-// Mock functions for test control
+// Test control functions
 void mock_set_wall(bool has_wall) {
     mock_wall = has_wall;
 }
@@ -23,11 +31,21 @@ void mock_set_visit_count(int count) {
 void mock_reset_state() {
     mock_wall = false;
     mock_visit_count = 0;
+    robot_state = STARTUP;
+    first_run = true;
+    current_pos = {START_POS, START_POS};
+    facing_direction = 1;
+    rotations_checked = 0;
+    best_direction = -1;
 }
 
-// Mock implementation of original functions
+// Mock implementations
 bool checkObstacle(QPointF pos, int direction) {
     return mock_wall;
+}
+
+int getVisitCount(coordinate pos) {
+    return mock_visit_count;
 }
 
 void ROS_INFO(const char* format, ...) {
