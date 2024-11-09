ed49b5e37d58059529179c07d4bc8a8f098184f6
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-09 02:24:24 -0500

    Update mashengl_student_maze.cpp

--- mashengl_student_maze.cpp
+++ mashengl_student_maze.cpp
@@ -158,17 +158,14 @@
 //   return result;
 // }
 
-#include "student.h"
-
 bool moveTurtle(QPointF& pos, int& orientation) {
-    // Current position
+    // Check for wall in current direction
     int x = static_cast<int>(std::floor(pos.x()));
     int y = static_cast<int>(std::floor(pos.y()));
-    
-    // Check wall based on current orientation
     int x1 = x, y1 = y;
     int x2 = x, y2 = y;
     
+    // Set up collision check based on orientation
     switch (orientation) {
         case 0: // WEST
             y2 = y + 1;
@@ -192,7 +189,7 @@ bool moveTurtle(QPointF& pos, int& orientation) {
     bool wall_detected = bumped(x1, y1, x2, y2);
     bool reached_goal = atend(x, y);
     
-    // Get next move from turtle logic
+    // Get next move
     turtleMove next_move = studentTurtleStep(wall_detected, reached_goal);
     
     if (!next_move.validAction) {
@@ -204,23 +201,21 @@ bool moveTurtle(QPointF& pos, int& orientation) {
         case FORWARD:
             if (!wall_detected) {
                 switch (orientation) {
-                    case 0: pos.setX(pos.x() - 1.0); break;
-                    case 1: pos.setY(pos.y() - 1.0); break;
-                    case 2: pos.setX(pos.x() + 1.0); break;
-                    case 3: pos.setY(pos.y() + 1.0); break;
+                    case 0: pos.setX(pos.x() - 1.0); break; // WEST
+                    case 1: pos.setY(pos.y() - 1.0); break; // NORTH
+                    case 2: pos.setX(pos.x() + 1.0); break; // EAST
+                    case 3: pos.setY(pos.y() + 1.0); break; // SOUTH
                 }
-                if (next_move.visitCount > 0) {
-                    displayVisits(next_move.visitCount);
-                }
+                displayVisits(next_move.visitCount);
             }
-            break;
-            
-        case LEFT:
-            orientation = (orientation + 3) % 4;
             break;
             
         case RIGHT:
             orientation = (orientation + 1) % 4;
+            break;
+            
+        case LEFT:
+            orientation = (orientation + 3) % 4;
             break;
     }
     
