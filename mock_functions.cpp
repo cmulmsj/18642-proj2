dd1ecb3a76bf6f2670b11ae640501a4e85d03629
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-09 05:35:39 -0500

    Update mock_functions.cpp

--- mock_functions.cpp
+++ mock_functions.cpp
@@ -6,6 +6,7 @@
 #include "student_mock.h"
 #include <cstdarg>
 #include <cstdio>
+#include <cstring>
 
 // State variables
 RobotState robot_state = STARTUP;
@@ -14,12 +15,39 @@ coordinate current_pos = {START_POS, START_POS};
 int facing_direction = 1;
 int rotations_checked = 0;
 int best_direction = -1;
+int min_visits = UINT8_MAX;
+int visit_grid[GRID_SIZE][GRID_SIZE] = {{0}};
 
 // Mock state variables
 static bool mock_wall = false;
 static int mock_visit_count = 0;
 
-// Mock implementations
+// Mock functions
+void mock_reset_state() {
+    robot_state = STARTUP;
+    first_run = true;
+    current_pos = {START_POS, START_POS};
+    facing_direction = 1;
+    rotations_checked = 0;
+    best_direction = -1;
+    min_visits = UINT8_MAX;
+    memset(visit_grid, 0, sizeof(visit_grid));
+    mock_wall = false;
+    mock_visit_count = 0;
+}
+
+void mock_set_wall(bool has_wall) {
+    mock_wall = has_wall;
+}
+
+bool mock_get_wall() {
+    return mock_wall;
+}
+
+void mock_set_visit_count(int count) {
+    mock_visit_count = count;
+}
+
 void mock_ros_info(const char* format, ...) {
     va_list args;
     va_start(args, format);
@@ -37,14 +65,6 @@ void mock_ros_error(const char* format, ...) {
     va_end(args);
 }
 
-void mock_set_wall(bool has_wall) {
-    mock_wall = has_wall;
-}
-
-void mock_set_visit_count(int count) {
-    mock_visit_count = count;
-}
-
 bool checkObstacle(QPointF pos, int direction) {
     return mock_wall;
 }
@@ -54,16 +74,8 @@ int getVisitCount(coordinate pos) {
 }
 
 void updateVisitMap(coordinate pos) {
-    mock_visit_count++;
-}
-
-void mock_reset_state() {
-    robot_state = STARTUP;
-    first_run = true;
-    current_pos = {START_POS, START_POS};
-    facing_direction = 1;
-    rotations_checked = 0;
-    best_direction = -1;
-    mock_wall = false;
-    mock_visit_count = 0;
+    if (pos.x >= 0 && pos.x < GRID_SIZE && pos.y >= 0 && pos.y < GRID_SIZE) {
+        visit_grid[pos.x][pos.y]++;
+        mock_visit_count = visit_grid[pos.x][pos.y];
+    }
 }
