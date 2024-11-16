1c0df3f92af79e921456ea824dc66126b1c2fe38
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-11-16 04:28:37 -0500

    Update mashengl_run.sh

--- mashengl_run.sh
+++ mashengl_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Function to kill all processes
 kill_processes() {
     for p in "$@"; do
@@ -8,29 +9,42 @@ kill_processes() {
             sleep 1
         fi
     done
+    # Parse monitor violations if monitor was running
+    if [ -n "$MONITOR_PID" ]; then
+        if [ -f "VIOLATIONS.txt" ]; then
+            echo "Monitor violations found:"
+            cat VIOLATIONS.txt
+        fi
+    fi
     echo "killed all processes, exiting"
     exit 0
 }
+
 # Check if maze number is provided
 if [ $# -ne 1 ]; then
     echo "Usage: $0 <maze_number>"
     exit 1
 fi
+
 MAZE_NUM=$1
 maze_file="m${MAZE_NUM}.maze"
+
 # Source the workspace setup
 source ~/catkin_ws/devel/setup.bash
+
 # Locate the ece642rtle directory
 turtledir=`rospack find ece642rtle`
 if [ -z "$turtledir" ]; then
     echo "Cannot locate package ece642rtle."
     exit 1
 fi
+
 # Check that maze file exists
 if [ ! -s "$turtledir/$maze_file" ]; then
     echo "Maze file $maze_file does not exist."
     exit 1
 fi
+
 # Run roscore if it is not already running
 ROSCORE_PID=""
 if [[ -z $(pgrep roscore) ]]; then
@@ -42,45 +56,46 @@ if [[ -z $(pgrep roscore) ]]; then
         exit 1
     fi
 fi
+
 # Have to kill BG process if user exits
 trap 'kill_processes $ROSCORE_PID' SIGINT
 sleep 5
+
 # Set maze file parameter
 rosparam set /maze_file "$maze_file"
-# Start monitor (added this section)
-cd "$turtledir/monitors"
+
+# Start turn monitor
+echo "Starting turn monitor..."
+cd $turtledir/monitors
 ./run_642_monitors.sh ece642rtle_turn_monitor &
-sleep 5
-    if [ -s "$m.output.tmp" ]; then
-        echo "" >> VIOLATIONS.txt
-        echo "Monitor $m Violations:" >> VIOLATIONS.txt
-        echo "" >> VIOLATIONS.txt
-        grep -C 5 "\[ WARN\]" $m.output.tmp >> VIOLATIONS.txt
-        m_viol=`grep "\[ WARN\]" $m.output.tmp | wc -l`
-        total_viol=$(( total_viol + m_viol))
-        rm $m.output.tmp
-        echo "" >> VIOLATIONS.txt
-    fi
+MONITOR_PID=$!
+sleep 2
+
 # Node that displays the maze and runs the turtle
 rosrun ece642rtle ece642rtle_node&
 TURTLE_PID=$!
 sleep 1
 if [[ -z $(pgrep ece642rtle_node) ]]; then
     echo "Error launching ece642rtle_node"
-    kill_processes $ROSCORE_PID
+    kill_processes $MONITOR_PID $ROSCORE_PID
     exit 1
 fi
+
 # Have to kill BG processes if user exits
-trap 'kill_processes $TURTLE_PID $ROSCORE_PID' SIGINT
+trap 'kill_processes $MONITOR_PID $TURTLE_PID $ROSCORE_PID' SIGINT
 sleep 9
+
 # Student node
 rosrun ece642rtle ece642rtle_student&
 STUDENT_PID=$!
+
 # Have to kill BG processes if user exits
-trap 'kill_processes $STUDENT_PID $TURTLE_PID $ROSCORE_PID' SIGINT
+trap 'kill_processes $STUDENT_PID $MONITOR_PID $TURTLE_PID $ROSCORE_PID' SIGINT
+
 # Wait for Ctrl+C
 while [ 1 -eq 1 ]; do
     sleep 30
 done
+
 # Return to home directory
 cd ~
