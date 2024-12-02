296355b2c83f12217bbe2e4f52d144413d6c5ed4
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-12-01 21:45:07 -0500

    Create mashengl_atend_monitor.cpp

---
+++ monitor/mashengl_atend_monitor.cpp
@@ -0,0 +1,55 @@
+#include "monitor_interface.h"
+#include "monitor_utils.h"
+
+static Pose current_pose;
+static bool maze_completed = false;
+static bool pose_initialized = false;
+
+void tickInterrupt(ros::Time t) {
+    // Output success message to flush warnings if maze is completed
+    if (maze_completed) {
+        ROS_WARN("Successful atEnd");
+    }
+}
+
+void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
+    current_pose = {x, y};
+    pose_initialized = true;
+    ROS_INFO("[[%ld ns]] Position updated: (%d,%d)", t.toNSec(), x, y);
+}
+
+void visitInterrupt(ros::Time t, int visits) {
+    // Not needed for atend monitoring
+}
+
+void bumpInterrupt(ros::Time t, int x1, int y1, int x2, int y2, bool bumped) {
+    // Not needed for atend monitoring
+}
+
+void atEndInterrupt(ros::Time t, int x, int y, bool atEnd) {
+    if (!pose_initialized) {
+        ROS_WARN("VIOLATION: atEnd check before position initialization at time %ld", t.toNSec());
+        return;
+    }
+
+    // Check if atEnd call matches current position
+    if (x != current_pose.x || y != current_pose.y) {
+        ROS_WARN("VIOLATION: atEnd check at (%d,%d) while turtle is at (%d,%d) at time %ld", 
+                 x, y, current_pose.x, current_pose.y, t.toNSec());
+    } else {
+        ROS_INFO("[[%ld ns]] Valid atEnd check at (%d,%d)", t.toNSec(), x, y);
+    }
+
+    // Update completion status
+    if (atEnd) {
+        maze_completed = true;
+    }
+}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "mashengl_atend_monitor");
+    MonitorUtils::logMonitorStart("AtEnd Monitor (mashengl)");
+    ros::NodeHandle nh;
+    ros::spin();
+    return 0;
+}
