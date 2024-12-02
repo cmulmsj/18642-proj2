af6097d6d25fdeef302fe70b25ec242c9dfe8f6f
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-12-01 22:58:45 -0500

    Update mashengl_turn_monitor.cpp

--- monitor/mashengl_turn_monitor.cpp
+++ monitor/mashengl_turn_monitor.cpp
@@ -1,18 +1,10 @@
-/*
- * STUDENT NAME: Mashengjun Li
- * ANDREW ID: mashengl
- * 
- * Monitor to check that turtle turns no more than 90 degrees per time step
- */
-
 #include "monitor_interface.h"
 
-// Track last orientation for comparison
-static Orientation last_orientation = NORTH;
+static Orientation last_orientation;
 static bool first_move = true;
 
-// Lookup table for checking valid 90-degree turns
-const bool valid_turns[4][4] = {
+// Lookup table for valid 90-degree turns
+static const bool valid_turns[4][4] = {
     //WEST  NORTH EAST  SOUTH (current)
     {true,  true,  false, true}, // WEST (previous)
     {true,  true,  true,  false},// NORTH
@@ -20,9 +12,8 @@ const bool valid_turns[4][4] = {
     {true,  false, true,  true}  // SOUTH
 };
 
-/*
- * Check orientation changes to ensure no more than 90-degree turns
- */
+void tickInterrupt(ros::Time t) {}
+
 void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
     if (first_move) {
         first_move = false;
@@ -31,22 +22,29 @@ void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
         return;
     }
 
-    // Check if turn is valid using lookup table
-    if (!valid_turns[last_orientation][o] && last_orientation != o) {
-        ROS_WARN("VIOLATION: Invalid turn from %d to %d - turn greater than 90 degrees!", 
-                 last_orientation, o);
-        ROS_INFO("Time: %ld ns, Position: (%d,%d)", t.toNSec(), x, y);
-    }
-
-    // Update last orientation
+    // Check if orientation changed
     if (last_orientation != o) {
-        ROS_INFO("[[%ld ns]] Turn detected: %d -> %d", t.toNSec(), last_orientation, o);
-        last_orientation = o;
+        if (!valid_turns[last_orientation][o]) {
+            ROS_WARN("VIOLATION: Invalid turn from %d to %d - turn greater than 90 degrees at time %ld!", 
+                     last_orientation, o, t.toNSec());
+        } else {
+            ROS_INFO("[[%ld ns]] Valid turn from %d to %d", t.toNSec(), last_orientation, o);
+        }
     }
+    
+    last_orientation = o;
 }
 
-// Required empty handlers for other interrupts
-void tickInterrupt(ros::Time t) {}
 void visitInterrupt(ros::Time t, int visits) {}
+
 void bumpInterrupt(ros::Time t, int x1, int y1, int x2, int y2, bool bumped) {}
+
 void atEndInterrupt(ros::Time t, int x, int y, bool atEnd) {}
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "mashengl_turn_monitor");
+    ROS_WARN("Monitor Turn Monitor (mashengl) is running at %s", ctime(0));
+    ros::NodeHandle nh;
+    ros::spin();
+    return 0;
+}
