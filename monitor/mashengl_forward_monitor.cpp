8fe5c47047b92f0864e3ffdc8d85400f3691b8c5
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-12-01 22:57:54 -0500

    Update mashengl_forward_monitor.cpp

--- monitor/mashengl_forward_monitor.cpp
+++ monitor/mashengl_forward_monitor.cpp
@@ -1,11 +1,24 @@
 #include "monitor_interface.h"
-#include "monitor_utils.h"
+#include <cstdlib>
 
 static Pose last_pose;
 static Orientation last_orientation;
 static bool has_turned = false;
 static bool first_move = true;
 static bool tick_active = false;
+
+static Orientation getMovementDirection(const Pose& from, const Pose& to) {
+    int dx = to.x - from.x;
+    int dy = to.y - from.y;
+    
+    if (dx == 1) return EAST;
+    if (dx == -1) return WEST;
+    if (dy == 1) return SOUTH;
+    if (dy == -1) return NORTH;
+    
+    ROS_ERROR("Invalid movement in getMovementDirection");
+    return NORTH;  // Default
+}
 
 void tickInterrupt(ros::Time t) {
     if (!tick_active) {
@@ -30,7 +43,7 @@ void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
     if (last_pose.x != x || last_pose.y != y) {
         // Verify movement direction matches orientation
         Pose current_pose = {x, y};
-        Orientation movement_dir = MonitorUtils::getMovementDirection(last_pose, current_pose);
+        Orientation movement_dir = getMovementDirection(last_pose, current_pose);
         
         if (last_orientation != movement_dir) {
             ROS_WARN("VIOLATION: Movement direction (%d) doesn't match orientation (%d) at time %ld", 
@@ -56,21 +69,13 @@ void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
     last_orientation = o;
 }
 
-void visitInterrupt(ros::Time t, int visits) {
-    // Not needed for forward monitoring
-}
-
-void bumpInterrupt(ros::Time t, int x1, int y1, int x2, int y2, bool bumped) {
-    // Not needed for forward monitoring
-}
-
-void atEndInterrupt(ros::Time t, int x, int y, bool atEnd) {
-    // Not needed for forward monitoring
-}
+void visitInterrupt(ros::Time t, int visits) {}
+void bumpInterrupt(ros::Time t, int x1, int y1, int x2, int y2, bool bumped) {}
+void atEndInterrupt(ros::Time t, int x, int y, bool atEnd) {}
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "forward_monitor");
-    MonitorUtils::logMonitorStart("Forward Monitor");
+    ros::init(argc, argv, "mashengl_forward_monitor");
+    ROS_WARN("Monitor Forward Monitor (mashengl) is running at %s", ctime(0));
     ros::NodeHandle nh;
     ros::spin();
     return 0;
