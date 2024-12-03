063ecd177abd9e2fe902b3c3938ca558d6cd5325
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-12-03 17:13:59 -0500

    Update mashengl_tick_monitor.cpp

--- monitor/mashengl_tick_monitor.cpp
+++ monitor/mashengl_tick_monitor.cpp
@@ -1,62 +1,153 @@
+// #include "monitor_interface.h"
+
+// // Track interrupt states between ticks
+// static bool pose_seen = false;
+// static bool visit_seen = false;
+// static bool bump_seen = false;
+// static bool tick_active = false;
+
+// void tickInterrupt(ros::Time t) {
+//     // Check if we had multiple calls of any interrupt type
+//     if (!tick_active) {
+//         tick_active = true;
+//         pose_seen = false;
+//         visit_seen = false;
+//         bump_seen = false;
+//         ROS_INFO("[[%ld ns]] New tick started", t.toNSec());
+//     } else {
+//         // Reset for next tick
+//         tick_active = false;
+//         ROS_INFO("[[%ld ns]] Tick completed", t.toNSec());
+//     }
+// }
+
+// void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
+//     if (!tick_active) {
+//         ROS_WARN("VIOLATION: Pose interrupt received outside of tick at time %ld", t.toNSec());
+//         return;
+//     }
+    
+//     if (pose_seen) {
+//         ROS_WARN("VIOLATION: Multiple pose interrupts within single tick at time %ld", t.toNSec());
+//     }
+//     pose_seen = true;
+//     ROS_INFO("[[%ld ns]] Pose interrupt received: x=%d, y=%d", t.toNSec(), x, y);
+// }
+
+// void visitInterrupt(ros::Time t, int visits) {
+//     if (!tick_active) {
+//         ROS_WARN("VIOLATION: Visit interrupt received outside of tick at time %ld", t.toNSec());
+//         return;
+//     }
+    
+//     if (visit_seen) {
+//         ROS_WARN("VIOLATION: Multiple visit interrupts within single tick at time %ld", t.toNSec());
+//     }
+//     visit_seen = true;
+//     ROS_INFO("[[%ld ns]] Visit interrupt received: visits=%d", t.toNSec(), visits);
+// }
+
+// void bumpInterrupt(ros::Time t, int x1, int y1, int x2, int y2, bool bumped) {
+//     if (!tick_active) {
+//         ROS_WARN("VIOLATION: Bump interrupt received outside of tick at time %ld", t.toNSec());
+//         return;
+//     }
+    
+//     if (bump_seen) {
+//         ROS_WARN("VIOLATION: Multiple bump interrupts within single tick at time %ld", t.toNSec());
+//     }
+//     bump_seen = true;
+//     ROS_INFO("[[%ld ns]] Bump interrupt received: (%d,%d)->(%d,%d) = %s", 
+//              t.toNSec(), x1, y1, x2, y2, bumped ? "true" : "false");
+// }
+
+// void atEndInterrupt(ros::Time t, int x, int y, bool atEnd) {
+//     // Not needed for tick monitoring
+// }
+
+
 #include "monitor_interface.h"
 
-// Track interrupt states between ticks
-static bool pose_seen = false;
-static bool visit_seen = false;
-static bool bump_seen = false;
-static bool tick_active = false;
+// Mailbox structure for incoming interrupts
+struct TickMonitorMailbox {
+    bool pose_received;
+    bool visit_received;
+    bool bump_received;
+    ros::Time last_time;
+};
+
+// Working state
+struct TickMonitorState {
+    bool tick_active;
+    bool pose_seen;
+    bool visit_seen;
+    bool bump_seen;
+};
+
+static TickMonitorMailbox mailbox = {};
+static TickMonitorState state = {};
+static bool first_run = true;
 
 void tickInterrupt(ros::Time t) {
-    // Check if we had multiple calls of any interrupt type
-    if (!tick_active) {
-        tick_active = true;
-        pose_seen = false;
-        visit_seen = false;
-        bump_seen = false;
+    if (first_run) {
+        first_run = false;
+        fprintf(stderr, "I'm running Tick Monitor (mashengl) to STDERR\n");
+        ROS_WARN("Monitor Tick Monitor (mashengl) is running");
+    }
+
+    if (!state.tick_active) {
+        // Start new tick
+        state.tick_active = true;
+        state.pose_seen = false;
+        state.visit_seen = false;
+        state.bump_seen = false;
         ROS_INFO("[[%ld ns]] New tick started", t.toNSec());
     } else {
-        // Reset for next tick
-        tick_active = false;
+        // End current tick
+        state.tick_active = false;
+        mailbox = {}; // Clear mailbox
         ROS_INFO("[[%ld ns]] Tick completed", t.toNSec());
     }
+    
+    mailbox.last_time = t;
 }
 
 void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
-    if (!tick_active) {
-        ROS_WARN("VIOLATION: Pose interrupt received outside of tick at time %ld", t.toNSec());
+    if (!state.tick_active) {
+        ROS_WARN("VIOLATION: [TICK] Pose interrupt received outside of tick");
         return;
     }
     
-    if (pose_seen) {
-        ROS_WARN("VIOLATION: Multiple pose interrupts within single tick at time %ld", t.toNSec());
+    if (state.pose_seen) {
+        ROS_WARN("VIOLATION: [TICK] Multiple pose interrupts within single tick");
     }
-    pose_seen = true;
+    state.pose_seen = true;
     ROS_INFO("[[%ld ns]] Pose interrupt received: x=%d, y=%d", t.toNSec(), x, y);
 }
 
 void visitInterrupt(ros::Time t, int visits) {
-    if (!tick_active) {
-        ROS_WARN("VIOLATION: Visit interrupt received outside of tick at time %ld", t.toNSec());
+    if (!state.tick_active) {
+        ROS_WARN("VIOLATION: [TICK] Visit interrupt received outside of tick");
         return;
     }
     
-    if (visit_seen) {
-        ROS_WARN("VIOLATION: Multiple visit interrupts within single tick at time %ld", t.toNSec());
+    if (state.visit_seen) {
+        ROS_WARN("VIOLATION: [TICK] Multiple visit interrupts within single tick");
     }
-    visit_seen = true;
+    state.visit_seen = true;
     ROS_INFO("[[%ld ns]] Visit interrupt received: visits=%d", t.toNSec(), visits);
 }
 
 void bumpInterrupt(ros::Time t, int x1, int y1, int x2, int y2, bool bumped) {
-    if (!tick_active) {
-        ROS_WARN("VIOLATION: Bump interrupt received outside of tick at time %ld", t.toNSec());
+    if (!state.tick_active) {
+        ROS_WARN("VIOLATION: [TICK] Bump interrupt received outside of tick");
         return;
     }
     
-    if (bump_seen) {
-        ROS_WARN("VIOLATION: Multiple bump interrupts within single tick at time %ld", t.toNSec());
+    if (state.bump_seen) {
+        ROS_WARN("VIOLATION: [TICK] Multiple bump interrupts within single tick");
     }
-    bump_seen = true;
+    state.bump_seen = true;
     ROS_INFO("[[%ld ns]] Bump interrupt received: (%d,%d)->(%d,%d) = %s", 
              t.toNSec(), x1, y1, x2, y2, bumped ? "true" : "false");
 }
