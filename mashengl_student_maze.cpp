82ca40dc30dc499f3edac1f52a34176578741983
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-12-05 00:31:23 -0500

    Update mashengl_student_maze.cpp

--- mashengl_student_maze.cpp
+++ mashengl_student_maze.cpp
@@ -140,16 +140,13 @@ bool checkObstacle(QPointF pos, int direction) {
 }
 
 bool moveTurtle(QPointF& pos, int& orientation) {
-    static const ros::Duration TICK_DURATION(0.5);
-
-    // Wait for previous tick's messages to be processed
-    ros::Duration(TICK_DURATION * 0.5).sleep();
+    static const ros::Duration TICK_DURATION(0.1); // Reduced from 0.5
     
     // Start new tick
     tick_state.is_active = true;
     tick_state.needs_check = true;
     
-    // Perform all state checks at tick start
+    // Perform state checks
     tick_state.wall_detected = checkObstacle(pos, orientation);
     tick_state.goal_reached = atend(static_cast<int>(std::floor(pos.x())), 
                                   static_cast<int>(std::floor(pos.y())));
@@ -159,10 +156,11 @@ bool moveTurtle(QPointF& pos, int& orientation) {
     
     if (!next_move.validAction) {
         tick_state.is_active = false;
+        ros::Duration(TICK_DURATION).sleep();
         return false;
     }
     
-    // Execute single action
+    // Execute action
     switch (next_move.action) {
         case FORWARD:
             if (!tick_state.wall_detected && !tick_state.goal_reached) {
@@ -194,10 +192,71 @@ bool moveTurtle(QPointF& pos, int& orientation) {
             return false;
     }
     
-    // Allow time for action messages to be processed within tick
-    ros::Duration(TICK_DURATION * 0.5).sleep();
-    
-    // End tick
+    // End tick with sleep
     tick_state.is_active = false;
+    ros::Duration(TICK_DURATION).sleep();
     return true;
 }
+
+// bool moveTurtle(QPointF& pos, int& orientation) {
+//     static const ros::Duration TICK_DURATION(0.5);
+
+//     // Wait for previous tick's messages to be processed
+//     ros::Duration(TICK_DURATION * 0.5).sleep();
+    
+//     // Start new tick
+//     tick_state.is_active = true;
+//     tick_state.needs_check = true;
+    
+//     // Perform all state checks at tick start
+//     tick_state.wall_detected = checkObstacle(pos, orientation);
+//     tick_state.goal_reached = atend(static_cast<int>(std::floor(pos.x())), 
+//                                   static_cast<int>(std::floor(pos.y())));
+    
+//     turtleMove next_move = studentTurtleStep(tick_state.wall_detected, 
+//                                            tick_state.goal_reached);
+    
+//     if (!next_move.validAction) {
+//         tick_state.is_active = false;
+//         return false;
+//     }
+    
+//     // Execute single action
+//     switch (next_move.action) {
+//         case FORWARD:
+//             if (!tick_state.wall_detected && !tick_state.goal_reached) {
+//                 switch (orientation) {
+//                     case 0: pos.setX(pos.x() - 1.0); break;
+//                     case 1: pos.setY(pos.y() - 1.0); break;
+//                     case 2: pos.setX(pos.x() + 1.0); break;
+//                     case 3: pos.setY(pos.y() + 1.0); break;
+//                     default:
+//                         ROS_ERROR("Invalid orientation");
+//                         tick_state.is_active = false;
+//                         return false;
+//                 }
+//                 displayVisits(next_move.visitCount);
+//             }
+//             break;
+            
+//         case RIGHT:
+//             orientation = (orientation + 1) % 4;
+//             break;
+            
+//         case LEFT:
+//             orientation = (orientation + 3) % 4;
+//             break;
+            
+//         default:
+//             ROS_ERROR("Invalid action");
+//             tick_state.is_active = false;
+//             return false;
+//     }
+    
+//     // Allow time for action messages to be processed within tick
+//     ros::Duration(TICK_DURATION * 0.5).sleep();
+    
+//     // End tick
+//     tick_state.is_active = false;
+//     return true;
+// }
