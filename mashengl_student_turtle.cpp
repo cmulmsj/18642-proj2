2a321ade654d6118a0fbf7ba1d1681180e7c6309
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-12-05 00:33:26 -0500

    Update mashengl_student_turtle.cpp

--- mashengl_student_turtle.cpp
+++ mashengl_student_turtle.cpp
@@ -146,6 +146,7 @@ static int facing_direction = 1; // Start facing NORTH
 static int scan_count = 0;
 static int best_direction = -1;
 static uint8_t min_visit_count = UINT8_MAX;
+static const size_t TIME_DELAY = 5;
 
 void updateVisitMap(coordinate pos) {
     if (pos.x < GRID_SIZE && pos.y < GRID_SIZE) {
@@ -177,6 +178,16 @@ coordinate getNextPosition(coordinate pos, int direction) {
 
 turtleMove studentTurtleStep(bool bumped_wall, bool at_goal) {
     turtleMove next_move = {FORWARD, true, 0};
+    
+    // Check delay counter and return if still waiting
+    if (tick_state.delay_counter > 0) {
+        tick_state.delay_counter--;
+        next_move.validAction = false;
+        return next_move;
+    }
+    
+    // Reset delay counter for next action
+    tick_state.delay_counter = TIME_DELAY;
     
     if (at_goal) {
         next_move.validAction = false;
@@ -244,3 +255,129 @@ turtleMove studentTurtleStep(bool bumped_wall, bool at_goal) {
     
     return next_move;
 }
+
+
+
+// /*
+//  * Originally by Philip Koopman (koopman@cmu.edu)
+//  * and Milda Zizyte (milda@cmu.edu)
+//  *
+//  * STUDENT NAME: Mashengjun Li
+//  * ANDREW ID: mashengl
+//  * LAST UPDATE: 12/02/2024
+//  */
+
+// #ifdef testing
+// #include "student_mock.h"
+// #define ROS_INFO mock_ros_info
+// #define ROS_ERROR mock_ros_error
+// #else
+// #include "student.h"
+// #include "ros/ros.h"
+
+// // Global state variables
+// static int visit_grid[GRID_SIZE][GRID_SIZE] = {{0}};
+// static bool first_run = true;
+// static coordinate current_pos = {START_POS, START_POS};
+// static int facing_direction = 1; // Start facing NORTH
+// static int scan_count = 0;
+// static int best_direction = -1;
+// static uint8_t min_visit_count = UINT8_MAX;
+
+// void updateVisitMap(coordinate pos) {
+//     if (pos.x < GRID_SIZE && pos.y < GRID_SIZE) {
+//         visit_grid[pos.x][pos.y]++;
+//     }
+// }
+
+// int getVisitCount(coordinate pos) {
+//     if (pos.x < GRID_SIZE && pos.y < GRID_SIZE) {
+//         return visit_grid[pos.x][pos.y];
+//     }
+//     return INT_MAX;
+// }
+
+// coordinate getNextPosition(coordinate pos, int direction) {
+//     coordinate next = pos;
+//     switch (direction) {
+//         case 0: next.x--; break; // WEST
+//         case 1: next.y--; break; // NORTH
+//         case 2: next.x++; break; // EAST
+//         case 3: next.y++; break; // SOUTH
+//         default:
+//             ROS_ERROR("Invalid direction");
+//             break;
+//     }
+//     return next;
+// }
+// #endif
+
+// turtleMove studentTurtleStep(bool bumped_wall, bool at_goal) {
+//     turtleMove next_move = {FORWARD, true, 0};
+    
+//     if (at_goal) {
+//         next_move.validAction = false;
+//         return next_move;
+//     }
+    
+//     // Initialize on first run
+//     if (first_run) {
+//         updateVisitMap(current_pos);
+//         next_move.visitCount = static_cast<uint8_t>(
+//             std::min(getVisitCount(current_pos), static_cast<int>(UINT8_MAX))
+//         );
+//         first_run = false;
+//         return next_move;
+//     }
+    
+//     // Scanning phase - limited to 4 scans
+//     if (scan_count < 4) {
+//         coordinate next_pos = getNextPosition(current_pos, facing_direction);
+//         uint8_t visit_count = static_cast<uint8_t>(
+//             std::min(getVisitCount(next_pos), static_cast<int>(UINT8_MAX))
+//         );
+        
+//         if (!bumped_wall && visit_count < min_visit_count) {
+//             min_visit_count = visit_count;
+//             best_direction = facing_direction;
+//         }
+        
+//         next_move.action = RIGHT;
+//         facing_direction = (facing_direction + 1) % 4;
+//         scan_count++;
+//     }
+//     // Movement phase - after scanning complete
+//     else {
+//         if (best_direction == -1) {
+//             // No valid direction found, reset scan
+//             scan_count = 0;
+//             min_visit_count = UINT8_MAX;
+//             next_move.action = RIGHT;
+//         }
+//         else if (facing_direction != best_direction) {
+//             // Turn to face best direction
+//             next_move.action = RIGHT;
+//             facing_direction = (facing_direction + 1) % 4;
+//         }
+//         else {
+//             // Move forward in best direction
+//             if (!bumped_wall) {
+//                 next_move.action = FORWARD;
+//                 current_pos = getNextPosition(current_pos, facing_direction);
+//                 updateVisitMap(current_pos);
+                
+//                 // Reset for next scan
+//                 scan_count = 0;
+//                 best_direction = -1;
+//                 min_visit_count = UINT8_MAX;
+//             }
+//         }
+//     }
+    
+//     // Always update the visit count in the move
+//     next_move.visitCount = static_cast<uint8_t>(
+//         std::min(getVisitCount(current_pos), static_cast<int>(UINT8_MAX))
+//     );
+    
+//     return next_move;
+// }
