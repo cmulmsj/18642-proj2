1ad1fdf20ed8931194be947748c5224c871348d5
cmulmsj <157081744+cmulmsj@users.noreply.github.com>
2024-12-05 19:14:45 -0500

    Update mashengl_atend_monitor.cpp

--- monitor/mashengl_atend_monitor.cpp
+++ monitor/mashengl_atend_monitor.cpp
@@ -1,19 +1,13 @@
 #include "monitor_interface.h"
 
-struct AtEndMonitorMailbox {
-    Pose latest_pose;
-    bool pose_updated;
-    bool maze_completed;
-};
-
 struct AtEndMonitorState {
-    Pose current_pose;
     bool initialized;
     bool maze_completed;
+    int current_x;
+    int current_y;
 };
 
-static AtEndMonitorMailbox mailbox = {};
-static AtEndMonitorState state = {};
+static AtEndMonitorState state = {false, false, 0, 0};
 static bool first_run = true;
 
 void tickInterrupt(ros::Time t) {
@@ -23,21 +17,16 @@ void tickInterrupt(ros::Time t) {
         ROS_WARN("Monitor AtEnd Monitor (mashengl) is running");
     }
 
-    // Process mailbox updates
-    if (mailbox.pose_updated) {
-        state.current_pose = mailbox.latest_pose;
-        mailbox.pose_updated = false;
-    }
-    
-    // Output success message to flush warnings if maze is completed
+    // Output success message if maze is completed
     if (state.maze_completed) {
         ROS_WARN("Successful atEnd");
     }
 }
 
 void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
-    mailbox.latest_pose = {x, y};
-    mailbox.pose_updated = true;
+    // Directly update state with new position
+    state.current_x = x;
+    state.current_y = y;
     state.initialized = true;
     
     ROS_INFO("[[%ld ns]] Position updated: (%d,%d)", t.toNSec(), x, y);
@@ -53,17 +42,15 @@ void atEndInterrupt(ros::Time t, int x, int y, bool atEnd) {
         return;
     }
 
-    if (x != state.current_pose.x || y != state.current_pose.y) {
+    if (x != state.current_x || y != state.current_y) {
         ROS_WARN("VIOLATION: [ATEND] Check at (%d,%d) while turtle is at (%d,%d)", 
-                 x, y, state.current_pose.x, state.current_pose.y);
+                 x, y, state.current_x, state.current_y);
     } else {
         ROS_INFO("[[%ld ns]] Valid atEnd check at (%d,%d)", t.toNSec(), x, y);
     }
 
     if (atEnd) {
         state.maze_completed = true;
-        mailbox.maze_completed = true;
     }
 }
-
 
